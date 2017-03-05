c74ce0f467fc75420f76a3d7d058a072959564f1
Dany Pinoy <dany.pinoy@pindanet.be>
2017-03-05 19:24:14 +0100

    Update wallpaper.sh

--- home/pi/wallpaper.sh
+++ home/pi/wallpaper.sh
@@ -7,13 +7,13 @@ PAGE=https://$SITE/wallpaper/downloads/random/android/800x480/index.html
 # Can we reach the site
 if ping -c 1 $SITE; then
   # Keep the 100 latest wallpapers
-  numfiles=`ls /media/data/var/www/html/background | wc -l`
+  numfiles=`ls /var/www/html/background | wc -l`
   while [ $((numfiles)) -gt 99 ]; do
-    oldest=`ls -t /media/data/var/www/html/background/* | tail -1`
+    oldest=`ls -t /var/www/html/background/* | tail -1`
     rm $oldest
-    numfiles=`ls /media/data/var/www/html/background | wc -l`
+    numfiles=`ls /var/www/html/background | wc -l`
   done
   # extract wallpaper 
   WOTD=`wget --user-agent="Mozilla/5.0 (X11; Linux x86_64; rv:49.0) Gecko/20100101 Firefox/49.0" -qO - $PAGE | grep "click here to download" | head -1 | sed -e "s,.*href=\",," -e "s,\",," | cut -d '>' -f 1`
-  wget --user-agent="Mozilla/5.0 (X11; Linux x86_64; rv:49.0) Gecko/20100101 Firefox/49.0" --directory-prefix=/media/data/var/www/html/background/ https://$SITE$WOTD
+  wget --user-agent="Mozilla/5.0 (X11; Linux x86_64; rv:49.0) Gecko/20100101 Firefox/49.0" --directory-prefix=/var/www/html/background/ https://$SITE$WOTD
 fi
