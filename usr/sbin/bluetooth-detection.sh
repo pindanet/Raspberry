8866e4204d463f509d4e4a2c63cc8368498b6e85
Dany Pinoy <dany.pinoy@pindanet.be>
2017-04-16 18:12:18 +0200

    Update bluetooth-detection.sh

--- usr/sbin/bluetooth-detection.sh
+++ usr/sbin/bluetooth-detection.sh
@@ -6,10 +6,12 @@
 # sudo hcitool info MA:C-:ad:dr:es:BT | md5sum | awk '{ print $1 }' >> bluetooth.detection
 # sudo mv bluetooth.detection /var/www/html/ /var/www/html/data/
 
-bluetooth=`cat /var/www/html/data/bluetooth.detection`
-info=`hcitool info ${bluetooth:0:17} | md5sum | awk '{ print $1 }'`
-if [ ${bluetooth:17:32} == $info ]; then
-  echo 0 > /sys/class/backlight/rpi_backlight/bl_power
-else
-  echo 1 > /sys/class/backlight/rpi_backlight/bl_power
-fi
+while IFS='' read -r bluetooth || [[ -n "$bluethooth" ]]; do
+# bluetooth=`cat /var/www/html/data/bluetooth.detection`
+  info=`hcitool info ${bluetooth:0:17} | md5sum | awk '{ print $1 }'`
+  if [ ${bluetooth:17:32} == $info ]; then
+    echo 0 > /sys/class/backlight/rpi_backlight/bl_power
+    exit
+  fi
+done < "/var/www/html/data/bluetooth.detection"
+echo 1 > /sys/class/backlight/rpi_backlight/bl_power
