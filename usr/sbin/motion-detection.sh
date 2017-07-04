b1337ba7ab744f9f7579ed9e8756078d6c803617
Dany Pinoy <dany.pinoy@pindanet.be>
2017-07-04 20:11:28 +0200

    Update motion-detection.sh

--- usr/sbin/motion-detection.sh
+++ usr/sbin/motion-detection.sh
@@ -3,18 +3,18 @@
 
 home=`cat /sys/class/backlight/rpi_backlight/bl_power`
 if [ $home == "1" ]; then # not home
-  mv /var/www/html/alarm/foto1.png /var/www/html/alarm/foto.png
-  raspistill -n -w 800 -h 480 -o /var/www/html/alarm/foto1.png
-  compare -fuzz 70% -metric ae /var/www/html/alarm/foto.png /var/www/html/alarm/foto1.png /var/www/html/alarm/diff.png 2> /var/www/html/alarm/foto_diff.txt
-  DIFF="$(cat /var/www/html/alarm/foto_diff.txt)"
+  mv /var/www/html/motion/foto1.png /var/www/html/motion/foto.png
+  raspistill -n -w 800 -h 480 -o /var/www/html/motion/foto1.png
+  compare -fuzz 50% -metric ae /var/www/html/motion/foto.png /var/www/html/motion/foto1.png /var/www/html/motion/diff.png 2> /var/www/html/motion/foto_diff.txt
+  DIFF="$(cat /var/www/html/motion/foto_diff.txt)"
   echo $DIFF
-  if [ "$DIFF" -gt "1000" ]; then
-    echo "Alarm"
-    cp /var/www/html/alarm/foto1.png "/var/www/html/alarm/fotos/`date +"%R %d %m %Y"`.png"
-  else
-    echo "No Alarm"
+  if [ "$DIFF" -gt "20" ]; then
+#    echo "Alarm"
+    cp /var/www/html/motion/foto1.png "/var/www/html/motion/fotos/`date +"%R %d %m %Y"`.png"
+#  else
+#    echo "No Alarm"
   fi
 else
-  echo "HOME"
-  raspistill -n -w 800 -h 480 -o /var/www/html/alarm/foto.png
+#  echo "HOME"
+  raspistill -n -w 800 -h 480 -o /var/www/html/motion/foto.png
 fi
