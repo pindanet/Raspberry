637e775bb975b0a93ed97c379528ad0bf0572078
Dany Pinoy <dany.pinoy@pindanet.be>
2017-07-16 19:37:08 +0200

    Update bluetooth-detection.sh

--- usr/sbin/bluetooth-detection.sh
+++ usr/sbin/bluetooth-detection.sh
@@ -11,11 +11,11 @@ while IFS='' read -r bluetooth || [[ -n "$bluethooth" ]]; do
   info=`hcitool info ${bluetooth:0:17} | md5sum`
   if [ ${bluetooth:17:32} == ${info:0:32} ]; then
     echo 0 > /sys/class/backlight/rpi_backlight/bl_power
-    echo 0 > /sys/class/leds/led0/brightness
-    echo 0 > /sys/class/leds/led1/brightness
+    echo 255 > /sys/class/leds/led0/brightness
+    echo 255 > /sys/class/leds/led1/brightness
     exit
   fi
 done < "/var/www/html/data/bluetooth.detection"
 echo 1 > /sys/class/backlight/rpi_backlight/bl_power
-echo 255 > /sys/class/leds/led0/brightness
-echo 255 > /sys/class/leds/led1/brightness
+echo 0 > /sys/class/leds/led0/brightness
+echo 0 > /sys/class/leds/led1/brightness
