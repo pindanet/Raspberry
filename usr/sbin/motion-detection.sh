9d69ce60260ee53b5697f6076b141b9894acb56c
Dany Pinoy <dany.pinoy@pindanet.be>
2017-07-18 19:51:26 +0200

    Update motion-detection.sh

--- usr/sbin/motion-detection.sh
+++ usr/sbin/motion-detection.sh
@@ -2,6 +2,11 @@
 # PiCam Motion Detection
 
 home=`cat /sys/class/backlight/rpi_backlight/bl_power`
+DIFF="$(cat /var/www/html/motion/foto_diff.txt)"
+if [ "$DIFF" -gt "20" ]; then # Alarm uitschakelen
+  python /home/pi/rfxcmd_gc-master/rfxcmd.py -d /dev/ttyUSB0 -s "0B 11 00 01 01 41 53 86 0D 00 00 80"
+  echo "0" > /var/www/html/motion/foto_diff.txt
+fi
 if [ $home == "1" ]; then # not home
   if [ -f /var/www/html/motion/foto.png ]; then # Motion detection active
     mv /var/www/html/motion/foto1.png /var/www/html/motion/foto.png
@@ -13,8 +18,6 @@ if [ $home == "1" ]; then # not home
       cp /var/www/html/motion/foto1.png "/var/www/html/motion/fotos/`date +"%Y %m %d %R"`.png"
       # Alarm laten afgaan
       python /home/pi/rfxcmd_gc-master/rfxcmd.py -d /dev/ttyUSB0 -s "0B 11 00 00 01 41 53 86 0D 01 0F 80"
-    else # Alarm uitschakelen
-      python /home/pi/rfxcmd_gc-master/rfxcmd.py -d /dev/ttyUSB0 -s "0B 11 00 01 01 41 53 86 0D 00 00 80"
     fi
   else # Activate motion detection
     raspistill -n -w 800 -h 480 -o /var/www/html/motion/foto.png
