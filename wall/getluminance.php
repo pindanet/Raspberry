08ab64decb374fc1edac71656c7751f88b71cf4e
Dany Pinoy <dany.pinoy@pindanet.be>
2018-05-21 17:46:28 +0200

    Update getluminance.php

--- wall/getluminance.php
+++ wall/getluminance.php
@@ -1,37 +1,12 @@
 <?php
 $context = [ 'http' => [ 'method' => 'GET' ], 'ssl' => [ 'verify_peer' => false, 'allow_self_signed'=> true, 'verify_peer_name' => false ] ];
 $context = stream_context_create($context);
-$resp->luminance = intval(file_get_contents('https://rpipindanet.local/motion/luminance.txt', false, $context));
 
-if ($resp->luminance == 0) {
-  $resp->luminance = 150;
-  $resp->max = 200;
-  $resp->min = 100;
-  echo json_encode($resp);
-  exit();
+$luminance = json_decode(file_get_contents('https://rpipindanet.local/motion/luminance.json', false, $context));
+if ($luminance == NULL) {
+  $luminance->luminance = 150;
+  $luminance->max = 200;
+  $luminance->min = 100;
 }
-
-$filename = "data/max.luminance";
-if (file_exists($filename)) {
-  $resp->max = intval(file_get_contents($filename));
-} else {
-  $resp->max = $resp->luminance;
-  file_put_contents($filename, $resp->max);
-}
-if ($resp->luminance > $resp->max) {
-  file_put_contents($filename, $resp->luminance);
-}
-
-$filename = "data/min.luminance";
-if (file_exists($filename)) {
-  $resp->min = intval(file_get_contents($filename));
-} else {
-  $resp->min = $resp->luminance;
-  file_put_contents($filename, $resp->min);
-}
-if ($resp->luminance < $resp->min) {
-  file_put_contents($filename, $resp->luminance);
-}
-
-echo json_encode($resp);
+echo json_encode($luminance);
 ?>
