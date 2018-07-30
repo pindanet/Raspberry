d9ad035a3b81c6054b052d3f75a6712db3778a25
Dany Pinoy <dany.pinoy@pindanet.be>
2018-07-30 12:34:17 +0200

    Update alarm_install.sh

--- alarm_install.sh
+++ alarm_install.sh
@@ -48,9 +48,9 @@ passwd
 sudo update-rc.d ssh enable
 sudo invoke-rc.d ssh start
 
-printf '\033[1;37;40mOn the main computer: ssh-copy-id -i ~/.ssh/id_rsa.pub pi@$NEW_HOSTNAME.local\n\033[0m' # Witte letters op zwarte achtergrond
-printf '\033[1;37;40mOn the domotica controller: ssh-copy-id -i ~/.ssh/id_rsa.pub pi@$NEW_HOSTNAME.local\n\033[0m' # Witte letters op zwarte achtergrond
-printf '\033[1;32;40mPress key to secure ssh.\033[0m' # Groene letters op zwarte achtergrond
+printf "\033[1;37;40mOn the main computer: ssh-copy-id -i ~/.ssh/id_rsa.pub pi@$NEW_HOSTNAME.local\n\033[0m" # Witte letters op zwarte achtergrond
+printf "\033[1;37;40mOn the domotica controller: ssh-copy-id -i ~/.ssh/id_rsa.pub pi@$NEW_HOSTNAME.local\n\033[0m" # Witte letters op zwarte achtergrond
+printf "\033[1;32;40mPress key to secure ssh.\033[0m" # Groene letters op zwarte achtergrond
 read Keypress
 
 #sudo sed -i "s/^.*PasswordAuthentication yes/PasswordAuthentication no/g" /etc/ssh/sshd_config
