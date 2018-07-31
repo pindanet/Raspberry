6e8f04547ff71c4a94912697779f6b346b39ba9b
Dany Pinoy <dany.pinoy@pindanet.be>
2018-07-31 13:50:34 +0200

    Update alarm_install.sh

--- alarm_install.sh
+++ alarm_install.sh
@@ -6,62 +6,66 @@ TIMEZONE="Europe/Brussels"
 COUNTRY="BE"
 NEW_HOSTNAME="rpialarm"
 
+if [ $HOSTNAME != $NEW_HOSTNAME ]; then
+# Configure  and upgrade
 # Change locale
-if ! LOCALE_LINE="$(grep "^$LOCALE " /usr/share/i18n/SUPPORTED)"; then
-  printf '\033[1;31;40mLOCALE %s not supported.\n\033[0m' $LOCALE # Rode letters op zwarte achtergrond
-  exit
-fi
-ENCODING="$(echo $LOCALE_LINE | cut -f2 -d " ")"
-echo "$LOCALE $ENCODING" | sudo tee /etc/locale.gen
-sudo sed -i "s/^\s*LANG=\S*/LANG=$LOCALE/" /etc/default/locale
+  if ! LOCALE_LINE="$(grep "^$LOCALE " /usr/share/i18n/SUPPORTED)"; then
+    printf '\033[1;31;40mLOCALE %s not supported.\n\033[0m' $LOCALE # Rode letters op zwarte achtergrond
+    exit
+  fi
+  ENCODING="$(echo $LOCALE_LINE | cut -f2 -d " ")"
+  echo "$LOCALE $ENCODING" | sudo tee /etc/locale.gen
+  sudo sed -i "s/^\s*LANG=\S*/LANG=$LOCALE/" /etc/default/locale
 
-sudo dpkg-reconfigure -f noninteractive locales
+  sudo dpkg-reconfigure -f noninteractive locales
 
 # Change timezone
-if [ ! -f "/usr/share/zoneinfo/$TIMEZONE" ]; then
-  printf '\033[1;31;40mTIMEZONE %s not supported.\n\033[0m' $TIMEZONE # Rode letters op zwarte achtergrond
-  exit
-fi
-sudo rm /etc/localtime
-echo "$TIMEZONE" | sudo tee /etc/timezone
-sudo dpkg-reconfigure -f noninteractive tzdata
+  if [ ! -f "/usr/share/zoneinfo/$TIMEZONE" ]; then
+    printf '\033[1;31;40mTIMEZONE %s not supported.\n\033[0m' $TIMEZONE # Rode letters op zwarte achtergrond
+    exit
+  fi
+  sudo rm /etc/localtime
+  echo "$TIMEZONE" | sudo tee /etc/timezone
+  sudo dpkg-reconfigure -f noninteractive tzdata
 
 # Change WiFi country
-if [ -e /etc/wpa_supplicant/wpa_supplicant.conf ]; then
-  if sudo grep -q "^country=" /etc/wpa_supplicant/wpa_supplicant.conf ; then
-    sudo sed -i --follow-symlinks "s/^country=.*/country=$COUNTRY/g" /etc/wpa_supplicant/wpa_supplicant.conf
+  if [ -e /etc/wpa_supplicant/wpa_supplicant.conf ]; then
+    if sudo grep -q "^country=" /etc/wpa_supplicant/wpa_supplicant.conf ; then
+      sudo sed -i --follow-symlinks "s/^country=.*/country=$COUNTRY/g" /etc/wpa_supplicant/wpa_supplicant.conf
+    else
+      sudo sed -i --follow-symlinks "1i country=$COUNTRY" /etc/wpa_supplicant/wpa_supplicant.conf
+    fi
   else
-    sudo sed -i --follow-symlinks "1i country=$COUNTRY" /etc/wpa_supplicant/wpa_supplicant.conf
+    echo "country=$COUNTRY" | sudo tee /etc/wpa_supplicant/wpa_supplicant.conf
   fi
-else
-  echo "country=$COUNTRY" | sudo tee /etc/wpa_supplicant/wpa_supplicant.conf
-fi
 
 # Change hostname
-CURRENT_HOSTNAME=`cat /etc/hostname | tr -d " \t\n\r"`
-echo $NEW_HOSTNAME | sudo tee /etc/hostname
-sudo sed -i "s/127.0.1.1.*$CURRENT_HOSTNAME/127.0.1.1\t$NEW_HOSTNAME/g" /etc/hosts
+  CURRENT_HOSTNAME=`cat /etc/hostname | tr -d " \t\n\r"`
+  echo $NEW_HOSTNAME | sudo tee /etc/hostname
+  sudo sed -i "s/127.0.1.1.*$CURRENT_HOSTNAME/127.0.1.1\t$NEW_HOSTNAME/g" /etc/hosts
 
 # Change password
-passwd
+  passwd
 
 # enable ssh
-sudo update-rc.d ssh enable
-sudo invoke-rc.d ssh start
+  sudo update-rc.d ssh enable
+  sudo invoke-rc.d ssh start
 
+# Upgrade
+  sudo apt update
+  sudo apt dist-upgrade -y
+  sudo apt autoremove -y
+
+#sudo wget -P /var/www/html https://raw.githubusercontent.com/mourner/suncalc/master/suncalc.js
+
+# Restart Raspberry Pi
+  sudo shutdown -r now
+  sleep 1m
+fi
+# SSH keydistribution
 printf "\033[1;37;40mOn the main computer: ssh-copy-id -i ~/.ssh/id_rsa.pub pi@$NEW_HOSTNAME.local\n\033[0m" # Witte letters op zwarte achtergrond
 printf "\033[1;37;40mOn the domotica controller: ssh-copy-id -i ~/.ssh/id_rsa.pub pi@$NEW_HOSTNAME.local\n\033[0m" # Witte letters op zwarte achtergrond
 printf "\033[1;32;40mPress key to secure ssh.\033[0m" # Groene letters op zwarte achtergrond
 read Keypress
 
 #sudo sed -i "s/^.*PasswordAuthentication yes/PasswordAuthentication no/g" /etc/ssh/sshd_config
-
-# Upgrade
-sudo apt update
-sudo apt dist-upgrade -y
-sudo apt autoremove -y
-
-#sudo wget -P /var/www/html https://raw.githubusercontent.com/mourner/suncalc/master/suncalc.js
-
-# Restart Raspberry Pi
-sudo shutdown -r now
