d63e9eda84596279eaaeee692b4164a4e2daba63
Dany Pinoy <dany.pinoy@pindanet.be>
2018-08-13 16:45:03 +0200

    Update and rename getjson.php to getPresHumiTemp.php

--- wall/getPresHumiTemp.php
+++ wall/getPresHumiTemp.php
@@ -1,7 +1,6 @@
 <?php
 $context = [ 'http' => [ 'method' => 'GET' ], 'ssl' => [ 'verify_peer' => false, 'allow_self_signed'=> true, 'verify_peer_name' => false ] ];
 $context = stream_context_create($context);
-$resp = file_get_contents('https://rpipindanet.local/data/thermostat.json', false, $context);
-//$json = base64_decode ($resp);
+$resp = file_get_contents('https://rpipindanet.local/data/PresHumiTemp', false, $context);
 echo $resp;
 ?>
