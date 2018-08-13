b6eebd752f541ba9531a05c22d6f9fc419c246af
Dany Pinoy <dany.pinoy@pindanet.be>
2018-08-13 18:19:41 +0200

    Create PindaNetState.sh

---
+++ usr/sbin/PindaNetState.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Clean Chromium start
+sed -i 's/"exited_cleanly":false/"exited_cleanly":true/; s/"exit_type":"[^"]\+"/"exit_type":"Normal"/' /home/pi/.config/chromium/Default/Preferences
+sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/pi/.config/chromium/'Local State'
+
+state=$(cat /var/www/html/data/state)
+
+if [ "$state" == "awake" ]; then
+  # activate touchscreen and status led's
+  echo 0 > /sys/class/backlight/rpi_backlight/bl_power
+  echo 255 > /sys/class/leds/led0/brightness
+  echo 255 > /sys/class/leds/led1/brightness
+  exit
+elif [ "$state" == "sleep" ]; then
+  # deactivate touchscreen and status led's
+  echo 1 > /sys/class/backlight/rpi_backlight/bl_power
+  echo 0 > /sys/class/leds/led0/brightness
+  echo 0 > /sys/class/leds/led1/brightness
+fi
