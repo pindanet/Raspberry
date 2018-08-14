4d4fe797d451f3e7dcb898df54d18399d4a4685e
Dany Pinoy <dany.pinoy@pindanet.be>
2018-08-14 18:22:42 +0200

    Update PindaNetbluetoothscan.sh

--- alarm/PindaNetbluetoothscan.sh
+++ alarm/PindaNetbluetoothscan.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
+BTController="B8:27:EB:49:17:03"
 # Array bluetooth MAC addresses
 # Scan with: hcitool scan
 bluetooth=(94:0E:6B:F8:97:31 94:0E:6B:F8:97:30)
 
+sendFile () {
+  obexftp --bluetooth $BTController --channel 23 -p $1
+#  if [ $? != "0" ]; then
+#    echo "$(date): SendFile second attempt $1" >> /var/PindaNet/debug.txt
+#    obexftp --bluetooth $BTController --channel 23 -p $1
+#    if [ $? != "0" ]; then
+#      echo "$(date): SendFile second attempt $1 Failed" >> /var/PindaNet/debug.txt
+#    fi
+#  fi
+}
 heating () {
   if [ $1 == "on" ] && [ $(gpio -g read 16) -eq 1 ]; then
     gpio -g mode 16 out
@@ -33,22 +44,33 @@ if [ "$home" -gt "0" ]; then
   echo 1 > /sys/class/leds/led0/brightness
   # Heating off
   heating off "in empty room"
+  # Sleep state
+  if [ $(cat /var/PindaNet/state) == "awake" ]; then
+    echo "sleep" > /var/PindaNet/state
+    sendFile /var/PindaNet/state
+  fi
 else
   # Activate Pi Zero ACT LED
-  echo none rc-feedback kbd-scrolllock kbd-numlock kbd-capslock kbd-kanalock kbd-shiftlock kbd-altgrlock kbd-ctrllock kbd-altlock kbd-shiftllock kbd-shiftrlock kbd-ctrlllock kbd-ctrlrlock timer oneshot heartbeat backlight gpio cpu cpu0 default-on input panic mmc1 [mmc0] rfkill-any rfkill0 rfkill1 > /sys/class/leds/led0/trigger
+  echo mmc0 > /sys/class/leds/led0/trigger
   echo 0 > /sys/class/leds/led0/brightness
   read_bme280 --i2c-address 0x77 > /var/PindaNet/PresHumiTemp
-
-  if [ $(tail -1 /var/PindaNet/PresHumiTemp) == "on" ]; then
+  sendFile /var/PindaNet/PresHumiTemp
+  # Heating
+  if [ $(cat /var/PindaNet/heating) == "on" ]; then
     heating on "manual"
   elif [ $(cat /var/PindaNet/heating) == "off" ]; then
     heating off "manual"
   else
-    temp=$(cat /var/PindaNet/temp)
-    if [ "$res2" -lt 20 ]; then
+    temp=$(tail -1 /var/PindaNet/PresHumiTemp)
+    if [ "${temp%%.*}" -lt 20 ]; then
       heating on "auto"
     else
       heating off "auto"
     fi
+  fi
+  # Awake state
+  if [ $(cat /var/PindaNet/state) == "sleep" ]; then
+    echo "awake" > /var/PindaNet/state
+     sendFile /var/PindaNet/state
   fi
 fi
