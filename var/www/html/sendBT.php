9942b0d53590fd3253af94cfc8b96d863a8fdac6
Dany Pinoy <dany.pinoy@pindanet.be>
2018-08-19 19:59:21 +0200

    Create sendBT.php

---
+++ var/www/html/sendBT.php
@@ -0,0 +1,18 @@
+<?php
+$BTController="B8:27:EB:45:8B:BA";
+$value = htmlspecialchars($_POST["value"]);
+$file = htmlspecialchars($_POST["file"]);
+
+//ob_flush();
+//ob_start();
+//var_dump($command);
+//file_put_contents('data/debug', ob_get_flush());
+
+file_put_contents("/var/www/html/data/" . $file, $value . "\n");
+
+exec("/usr/bin/obexftp --bluetooth " . $BTController . " --channel 23 -p /var/www/html/data/" . $file, $output, $return);
+//foreach ($output as $line) {
+//  echo "$line\n";
+//}
+
+?>
