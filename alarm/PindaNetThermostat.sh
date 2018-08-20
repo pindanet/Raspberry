fbea50b50d660c374458956e34ced6ac510f6aa9
Dany Pinoy <dany.pinoy@pindanet.be>
2018-08-20 18:29:45 +0200

    Update PindaNetThermostat.sh

--- alarm/PindaNetThermostat.sh
+++ alarm/PindaNetThermostat.sh
@@ -20,16 +20,7 @@ elif [ $(cat /var/PindaNet/heating) == "off" ]; then
 fi
 
 if [ ! -f /var/PindaNet/thermostat ]; then
-#  cat > /var/PindaNet/thermostat <<EOF
-#0;09:30;20.00 0;21:25;-off- \
-#1;09:30;20.00 1;21:25;-off- \
-#2;09:30;20.00 2;21:25;-off- \
-#3;09:30;20.00 3;21:25;-off- \
-#4;09:30;20.00 4;21:25;-off- \
-#5;09:30;20.00 5;21:25;-off- \
-#6;09:30;20.00 6;21:25;-off-
-#EOF
-echo -n "0;09:30;20.00 0;21:25;-off- 1;09:30;20.00 1;21:25;-off- 2;09:30;20.00 2;21:25;-off- 3;09:30;20.00 3;21:25;-off- 4;09:30;20.00 4;21:25;-off- 5;09:30;20.00 5;21:25;-off- 6;09:30;20.00 6;21:25;-off-" > /var/PindaNet/thermostat
+  echo -n "0;09:30;20.00 0;21:25;-off- 1;09:30;20.00 1;21:25;-off- 2;09:30;20.00 2;21:25;-off- 3;09:30;20.00 3;21:25;-off- 4;09:30;20.00 4;21:25;-off- 5;09:30;20.00 5;21:25;-off- 6;09:30;20.00 6;21:25;-off-" > /var/PindaNet/thermostat
 fi
 thermostat=`cat /var/PindaNet/thermostat`
 weekday=$(date +%u)
@@ -41,15 +32,17 @@ thermostatTemp=${thermostat: -5}
 data=${thermostat:0:13}
 thermostat=${thermostat: 14}
 while [ "$data" != "" ]; do
-  if [[ "${data:0:1}" > "$weekday" ]]; then
-    break
-  fi
   if [[ "${data:2:5}" < "$now" ]]; then
     thermostatTemp=${data:8:5}
   fi 
+  if [[ "${data:0:1}" > "$weekday" ]]; then
+    break
+  fi
   data=${thermostat:0:13}
   thermostat=${thermostat: 14}
 done
+
+echo $thermostatTemp
 
 temp=$(tail -1 /var/PindaNet/PresHumiTemp)
 temp=${temp%% C*}
