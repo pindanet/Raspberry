c3d9188b242d98ad57b55c487391a40b5409ff42
Dany Pinoy <dany.pinoy@pindanet.be>
2018-09-09 19:49:27 +0200

    Delete autostart.sh

--- home/pi/.config/lxsession/LXDE-pi/autostart.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Start fullscreen browser
-xte 'sleep 12' 'mousemove 400 240' 'sleep 1' 'key F11' 'sleep 1' 'key F5' &
-firefox https://localhost &
-# Disable Screensaver
-xset s off
-xset -dpms
-xset s noblank
