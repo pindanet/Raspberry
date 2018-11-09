a339a0833d26028ca154f7836a58202e4f53d783
Dany Pinoy <dany.pinoy@pindanet.be>
2018-11-09 14:19:06 +0100

    Update getPresHumiTemp.php

--- wall/getPresHumiTemp.php
+++ wall/getPresHumiTemp.php
@@ -1,6 +1,6 @@
 <?php
 $context = [ 'http' => [ 'method' => 'GET' ], 'ssl' => [ 'verify_peer' => false, 'allow_self_signed'=> true, 'verify_peer_name' => false ] ];
 $context = stream_context_create($context);
-$resp = file_get_contents('https://rpipindanet.local/data/PresHumiTemp', false, $context);
+$resp = file_get_contents('https://rpipindanet/data/PresHumiTemp', false, $context);
 echo $resp;
 ?>
