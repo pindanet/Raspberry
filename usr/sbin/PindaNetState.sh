0642dff9a6488a7b432b287145ebcbd97154b732
Dany Pinoy <dany.pinoy@pindanet.be>
2019-02-18 18:39:52 +0100

    Update PindaNetState.sh

--- usr/sbin/PindaNetState.sh
+++ usr/sbin/PindaNetState.sh
@@ -4,7 +4,33 @@
 #sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/pi/.config/chromium/'Local State'
 
 # Cleanup missed received bluetooth files
-find /var/www/html/data/bluetooth -mmin +5 -type f -delete
+#find /var/www/html/data/bluetooth -mmin +5 -type f -delete
+
+BTController="B8:27:EB:49:17:03"
+# Array bluetooth MAC addresses
+# Scan with: hcitool scan
+bluetooth=(94:0E:6B:F8:97:31 6C:24:83:B8:98:7B)
+
+home="1"
+for i in ${bluetooth[@]}; do
+  sdptool browse $i
+  if [ $? == 0 ]; then # home
+    home="0"
+    break
+  fi
+done
+
+if [ "$home" -gt "0" ]; then
+  # Sleep state
+  if [ $(cat /var/www/html/data/state) == "awake" ]; then
+    echo "sleep" > /var/www/html/data/state
+  fi
+else
+  # Awake state
+  if [ $(cat /var/www/html/data/state) == "sleep" ]; then
+    echo "awake" > /var/www/html/data/state
+  fi
+fi
 
 state=$(cat /var/www/html/data/state)
 
