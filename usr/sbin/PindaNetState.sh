66cc2720d92b85d0e89e7029d70afcc51cdfd0e1
Dany Pinoy <dany.pinoy@pindanet.be>
2019-04-10 19:53:47 +0200

    Update PindaNetState.sh

--- usr/sbin/PindaNetState.sh
+++ usr/sbin/PindaNetState.sh
@@ -13,43 +13,52 @@ now=$(date +%H:%M)
 raspistill -vf -hf -n -w 800 -h 480 -o /var/www/html/motion/day/$now.jpg
 find /var/www/html/motion/day/*.jpg -mtime +0 -type f -delete
 
-BTController="B8:27:EB:49:17:03"
+# Calculate brightness for screen backlight adjustment
+brightness=$(convert /var/www/html/motion/day/$now.jpg -colorspace gray -format "%[fx:100*mean]" info:)
+backlight=$(echo $brightness 1.27 | awk '{printf "%.0f\n",$1*$2}')
+echo $backlight | sudo tee /sys/class/backlight/rpi_backlight/brightness
+
+# deactivate status led's
+echo 0 > /sys/class/leds/led0/brightness
+echo 0 > /sys/class/leds/led1/brightness
+
+#BTController="B8:27:EB:49:17:03"
 # Array bluetooth MAC addresses
 # Scan with: hcitool scan
-bluetooth=(94:0E:6B:F8:97:31 7C:67:A2:C1:F8:F8)
+#bluetooth=(94:0E:6B:F8:97:31 7C:67:A2:C1:F8:F8)
 
-home="1"
-for i in ${bluetooth[@]}; do
-  sdptool browse $i
-  if [ $? == 0 ]; then # home
-    home="0"
-    break
-  fi
-done
+#home="1"
+#for i in ${bluetooth[@]}; do
+#  sdptool browse $i
+#  if [ $? == 0 ]; then # home
+#    home="0"
+#    break
+#  fi
+#done
 
-if [ "$home" -gt "0" ]; then
+#if [ "$home" -gt "0" ]; then
   # Sleep state
-  if [ $(cat /var/www/html/data/state) == "awake" ]; then
-    echo "sleep" > /var/www/html/data/state
-  fi
-else
+#  if [ $(cat /var/www/html/data/state) == "awake" ]; then
+#    echo "sleep" > /var/www/html/data/state
+#  fi
+#else
   # Awake state
-  if [ $(cat /var/www/html/data/state) == "sleep" ]; then
-    echo "awake" > /var/www/html/data/state
-  fi
-fi
+#  if [ $(cat /var/www/html/data/state) == "sleep" ]; then
+#    echo "awake" > /var/www/html/data/state
+#  fi
+#fi
 
-state=$(cat /var/www/html/data/state)
+#state=$(cat /var/www/html/data/state)
 
-if [ "$state" == "awake" ]; then
+#if [ "$state" == "awake" ]; then
   # activate touchscreen and status led's
-  echo 0 > /sys/class/backlight/rpi_backlight/bl_power
-  echo 255 > /sys/class/leds/led0/brightness
-  echo 255 > /sys/class/leds/led1/brightness
-  exit
-elif [ "$state" == "sleep" ]; then
+#  echo 0 > /sys/class/backlight/rpi_backlight/bl_power
+#  echo 255 > /sys/class/leds/led0/brightness
+#  echo 255 > /sys/class/leds/led1/brightness
+#  exit
+#elif [ "$state" == "sleep" ]; then
   # deactivate touchscreen and status led's
-  echo 1 > /sys/class/backlight/rpi_backlight/bl_power
-  echo 0 > /sys/class/leds/led0/brightness
-  echo 0 > /sys/class/leds/led1/brightness
-fi
+#  echo 1 > /sys/class/backlight/rpi_backlight/bl_power
+#  echo 0 > /sys/class/leds/led0/brightness
+#  echo 0 > /sys/class/leds/led1/brightness
+#fi
