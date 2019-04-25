cfe9bedf1fc96f7e58c51b077256325b4d538a16
Dany Pinoy <dany.pinoy@pindanet.be>
2019-04-25 14:30:09 +0200

    Update system.php

--- var/www/html/system.php
+++ var/www/html/system.php
@@ -38,17 +38,15 @@ case "rpiwall":
     ssh2_auth_pubkey_file($connection, 'pi',
                           '.newssh_keys/id_rsa.pub',
                           '.newssh_keys/id_rsa', '');
-//    $stream = ssh2_exec($connection, 'ls /var/www/html/background/');
     $stream = ssh2_exec($connection, "sudo shutdown -h now");
     stream_set_blocking($stream, true);
     $output = stream_get_contents($stream);
-//    echo "<pre>{$output}</pre>";
     fclose($stream);
     sleep(60); // wait 1 minute
-    exec("cd data; python /home/pi/rfxcmd_gc-master/rfxcmd.py -d /dev/ttyUSB0 -s \"0B 11 00 04 01 25 4A AE 0E 00 00 80\"");
+    exec("cd data; python /home/pi/rfxcmd_gc-master/rfxcmd.py -d /dev/ttyUSB0 -s \"0B 11 00 00 01 25 4A AE 0D 00 00 80\"");
     echo "Wall afgesloten en stroomtoevoer uitgeschakeld.";
   } else {
-    exec("cd data; python /home/pi/rfxcmd_gc-master/rfxcmd.py -d /dev/ttyUSB0 -s \"0B 11 00 02 01 25 4A AE 0E 01 0F 80\"");
+    exec("cd data; python /home/pi/rfxcmd_gc-master/rfxcmd.py -d /dev/ttyUSB0 -s \"0B 11 00 00 01 25 4A AE 0D 01 0F 80\"");
     echo "Stroomtoevoer naar Wall ingeschakeld.";
   }
   break;
@@ -56,5 +54,4 @@ case "saveThermostat":
 //  file_put_contents ("data/thermostat.json", $_POST["json"] . "\n");
   file_put_contents ("data/thermostat", base64_decode($_POST["json"]));
   break;
-
 endswitch ?>
