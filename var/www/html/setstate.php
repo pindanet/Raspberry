20378d3ff6ce52cffa0ddd4722ae98dde2e35d92
Dany Pinoy <dany.pinoy@pindanet.be>
2019-05-16 15:22:58 +0200

    Update setstate.php

--- var/www/html/setstate.php
+++ var/www/html/setstate.php
@@ -2,9 +2,15 @@
 $state = htmlspecialchars($_POST["state"]);
 
 exec("cat /sys/class/backlight/rpi_backlight/bl_power", $output, $return);
-echo $output[0];
 
 $myfile = fopen("/var/www/html/data/debug.txt", "a") or die("Unable to open file!");
 fwrite($myfile, date("l d/m/Y H:i:s: ") . $state . ".\n");
 fclose($myfile);
+
+if ($output[0] == 0 && $state == "sleep") {
+  exec("echo 1 | sudo /usr/bin/tee /sys/class/backlight/rpi_backlight/bl_power");
+}
+if ($output[0] == 1 && $state == "awake") {
+  exec("echo 0 | sudo /usr/bin/tee /sys/class/backlight/rpi_backlight/bl_power");
+}
 ?>
