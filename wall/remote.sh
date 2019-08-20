7144695adf0253a1189006f3ff612d6b5ce0e262
Dany Pinoy <dany.pinoy@pindanet.be>
2019-08-20 11:45:22 +0200

    Update remote.sh

--- wall/remote.sh
+++ wall/remote.sh
@@ -6,4 +6,18 @@ case "$command" in
   halt)
     shutdown -h now
     ;;
+  rsync)
+    rsync -aAXv --delete --relative /home/dany/wall_install.sh /usr/sbin/background.sh /var/www/ pindabackup::backup/rpiwall
+    touch /var/www/html/data/rsync.log
+    ;;
+  update)
+    apt-get clean -y
+    apt-get update -y
+    apt-get upgrade -y
+    cp /var/log/apt/history.log /var/www/html/data/update.log
+    ;;
+  clean)
+    rm /var/www/html/data/rsync.log
+    rm /var/www/html/data/update.log
+    ;;
 esac
