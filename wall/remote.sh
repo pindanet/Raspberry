bb440613d988a1b578742ca161cc5662acb6ea6d
Dany Pinoy <dany.pinoy@pindanet.be>
2019-08-22 16:07:41 +0200

    Update remote.sh

--- wall/remote.sh
+++ wall/remote.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 command=$(cat $1)
-cat $1 > /home/dany/remote.log
-rm $1
-case "$command" in
-  halt)
-    shutdown -h now
-    ;;
-  rsync)
-    rsync -aAXv --delete --relative /home/dany/wall_install.sh /usr/sbin/background.sh /var/www/ pindabackup::backup/rpiwall
-    touch /var/www/html/data/rsync.log
-    ;;
-  update)
-    apt-get clean -y
-    apt-get update -y
-    apt-get upgrade -y
-    cp /var/log/apt/history.log /var/www/html/data/update.log
-    ;;
-  clean)
-    rm /var/www/html/data/rsync.log
-    rm /var/www/html/data/update.log
-    ;;
-esac
+cat $1 > /root/remote.log
+if [ ${#command}  -lt 7 ]; then
+  rm $1
+  case "$command" in
+    halt)
+      shutdown -h now
+      ;;
+    rsync)
+      rsync -aAXv --delete --relative /home/dany/wall_install.sh /usr/sbin/background.sh /var/www/ pindabackup::backup/rpiwall > /root/rsync.log 2>&1
+      mv /root/rsync.log /var/www/html/remote/rsync.log
+      ;;
+    update)
+      apt-get clean -y > /root/update.log 2>&1
+      apt-get update -y >> /root/update.log 2>&1
+      apt-get upgrade -y >> /root/update.log 2>&1
+      mv /root/update.log /var/www/html/remote/update.log
+      ;;
+    clean)
+      rm /var/www/html/remote/rsync.log
+      rm /var/www/html/remote/update.log
+      ;;
+  esac
+fi
