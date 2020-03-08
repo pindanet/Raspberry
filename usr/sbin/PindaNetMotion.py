fe5355ccb748309f434329e6153ff910c3a64b75
Dany Pinoy <dany.pinoy@pindanet.be>
2020-03-08 19:56:52 +0100

    Update PindaNetMotion.py

--- usr/sbin/PindaNetMotion.py
+++ usr/sbin/PindaNetMotion.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
 from gpiozero import MotionSensor
+from picamera import PiCamera
 import time
+import os
+
+pir = MotionSensor(4)
+camera = PiCamera()
+camera.rotation = 180
+motionfolder = "/var/www/html/motion/fotos/"
 
 count = 0
 now = 0
@@ -10,7 +17,6 @@ def logme( str ):
   print (time.asctime( time.localtime(now)), str)
   return
 
-pir = MotionSensor(4)
 while True:
   pir.wait_for_motion()
   count += 1
@@ -18,9 +24,22 @@ while True:
   now = time.time()
   logme("Motion! " + time.asctime( time.localtime(now)))
   logme (str(round(now - previous)) + "s between previous detected movement " + str(count))
-  
-  for entry in os.scandir("/var/www/html/motion/fotos/"):
-    print(entry.name, entry.stat().st_ctime)
-  
+
+  files = []
+  for entry in os.scandir(motionfolder):
+    "print(entry.name, entry.stat().st_ctime)"
+    files.append(entry.name)
+
+  if len(files) > 100:
+    print(len(files))
+    files.sort()
+    oldest = motionfolder + files[0]
+    os.remove(oldest)
+    print (oldest + " removed")
+
+  filename = motionfolder + time.strftime("%Y %m %d %H:%M:%S") + ".jpg"
+  print(filename)
+  camera.capture(filename)
+
   pir.wait_for_no_motion()
   logme("No Motion!")
