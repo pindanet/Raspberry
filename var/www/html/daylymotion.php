cc9f8fb51ca6a9897b6c6ef1f62eccf4f916cbcd
Dany Pinoy <dany.pinoy@pindanet.be>
2020-03-09 19:53:46 +0100

    Update daylymotion.php

--- var/www/html/daylymotion.php
+++ var/www/html/daylymotion.php
@@ -1,5 +1,9 @@
 <?php
-$files = array_diff(scandir("motion/day/"), array('..', '.'));
+function pathPrefix(&$value,$key) {
+  $value="motion/fotos/$value";
+}
+$files = array_diff(scandir("motion/fotos/"), array('..', '.'));
 sort($files);
+array_walk($files,"pathPrefix");
 echo json_encode($files);
 ?>
