a8d4959f7a2fbc092a716e13f1f5c080d04a2980
Dany Pinoy <dany.pinoy@pindanet.be>
2020-04-28 14:43:08 +0200

    Update PindaNetThermostat.sh

--- domoticaController/usr/sbin/PindaNetThermostat.sh
+++ domoticaController/usr/sbin/PindaNetThermostat.sh
@@ -1,125 +1,310 @@
 #!/bin/bash
-LivingZuidOn="0B 11 00 01 01 41 53 86 01 01 0F 80"
-LivingZuidOff="0B 11 00 00 01 41 53 86 01 00 00 80"
-LivingNoordOn="0B 11 00 00 01 41 53 86 02 01 0F 80"
-LivingNoordOff="0B 11 00 01 01 41 53 86 02 00 00 80"
+# ToDo
+# sleep verwarming uitschakelen
+thermostatkitchendefault[0]="07:20 08:30 22.00"
+thermostatkitchendefault[2]="11:00 13:30 22.20"
+thermostatkitchendefault[1]="22:15 22:45 22.20"
+thermostatkitchendefault[3]="16:45 17:15 22.20"
+
+thermostatlivingdefault[0]="08:10 11:15 22.00"
+thermostatlivingdefault[2]="13:15 17:00 22.00"
+thermostatlivingdefault[1]="17:15 22:45 22.00"
+# thermostatlivingdefault[3]="16:45 17:15 22.00"
+
+timerfile="/var/www/html/data/timer"
+thermostatkitchenfile="/var/www/html/data/thermostatkitchen"
+thermostatlivingfile="/var/www/html/data/thermostatliving"
+PresHumiTempfile="/var/www/html/data/PresHumiTemp"
+
+# force default
+rm $timerfile
+rm $thermostatkitchenfile
+rm $thermostatlivingfile
+
+TestOn="tasmota_8be4af-1199 on"
+TestOff="tasmota_8be4af-1199 off"
+
+LivingZuidOn="0B1100010141538601010F80 on"
+LivingZuidOff="0B1100000141538601000080 off"
+LivingNoordOn="0B1100000141538602010F80 on"
+LivingNoordOff="0B1100010141538602000080 off"
+KeukenZuidOn="tasmota_a943fa-1018 on"
+KeukenZuidOff="tasmota_a943fa-1018 off"
+
+hysteresis="0.1"
+
 sendRF () {
+#  echo $1 $2
   if [ ! -f /var/www/html/data/mpc.txt ]; then
     touch /var/www/html/data/mpc.txt
   fi
-  python /home/pi/rfxcmd_gc-master/rfxcmd.py -d /dev/ttyUSB0 -s "$1"
+  tempfile="/tmp/${1:0:6}${1:8:10}${1:22:2}"
+  if [ ! -f "$tempfile" ]; then # initialize
+    if [ "$2" == "off" ]; then
+      echo 'on' > "$tempfile"
+    else
+      echo 'off' > "$tempfile"
+    fi
+  fi
+echo $tempfile $(cat "$tempfile") $2
+  if [ $(cat "$tempfile") == "off" ] && [ "$2" == "on" ]; then
+    python /home/pi/rfxcmd_gc-master/rfxcmd.py -d /dev/ttyUSB0 -s "$1"
+    echo 'on' > "$tempfile"
+  elif [ $(cat "$tempfile") == "on" ] && [ "$2" == "off" ]; then
+    python /home/pi/rfxcmd_gc-master/rfxcmd.py -d /dev/ttyUSB0 -s "$1"
+    echo 'off' > "$tempfile"
+  fi
   if [ -s /var/www/html/data/mpc.txt ]; then
     rm /var/www/html/data/mpc.txt
   fi
 }
-heating () {
-  if [ ! -f /var/www/html/data/heating ]; then
-    echo "off" > /var/www/html/data/heating
-    chown www-data:www-data /var/www/html/data/heating
+tasmota () {
+  if [ ! -f /tmp/$1 ]; then # initialize
+    wget -q http://$1/cm?cmnd=Power%20Off
+    echo '{"POWER":"OFF"}' > /tmp/$1
   fi
-  if [ ! -f /var/www/html/data/thermostatmode ]; then
-    echo "auto" > /var/www/html/data/thermostatmode
-    chown www-data:www-data /var/www/html/data/thermostatmode
-  fi
-#echo "on/off=$1 heating=$(cat /var/www/html/data/heating) mode=$(cat /var/www/html/data/thermostatmode)" >> /tmp/PindaNetDebug.txt
-  if [ $(cat /var/www/html/data/thermostatmode) == "auto" ]; then
-    if [ $1 == "on" ] && [ $(cat /var/www/html/data/heating) == "off" ]; then
-#      sendRF "0B 11 00 01 01 41 53 86 01 01 0F 80" # Living Zuid
-#      sendRF "0B 11 00 00 01 41 53 86 02 01 0F 80" # Living Noord
-      sendRF "$LivingZuidOn"
-      sendRF "$LivingNoordOn"
-#      python /home/pi/rfxcmd_gc-master/rfxcmd.py -d /dev/ttyUSB0 -s "0B 11 00 01 01 41 53 86 01 01 0F 80"
-      echo "on" > /var/www/html/data/heating
-      echo "$(date): Heating $2 on" >> /tmp/PindaNetDebug.txt
-    elif [ $1 == "off" ] && [ $(cat /var/www/html/data/heating) == "on" ]; then
-#      sendRF "0B 11 00 00 01 41 53 86 01 00 00 80" # LivingZuidOff
-#      sendRF "0B 11 00 01 01 41 53 86 02 00 00 80" # LivingNoordOff
-      sendRF "$LivingZuidOff"
-      sendRF "$LivingNoordOff"
-#      python /home/pi/rfxcmd_gc-master/rfxcmd.py -d /dev/ttyUSB0 -s "0B 11 00 00 01 41 53 86 01 00 00 80"
-      echo "off" > /var/www/html/data/heating
-      echo "$(date): Heating $2 off" >> /tmp/PindaNetDebug.txt
-    fi
+  if [ $2 == "on" ] && [ $(cat /tmp/$1) == '{"POWER":"OFF"}' ]; then
+    wget -q http://$1/cm?cmnd=Power%20On
+    echo $(wget -qO- http://$1/cm?cmnd=Power) > /tmp/$1
+    echo "$(date): $1 on" >> /tmp/PindaNetTimerDebug.txt
+  elif [ $2 == "off" ] && [ $(cat /tmp/$1) == '{"POWER":"ON"}' ]; then
+    wget -q http://$1/cm?cmnd=Power%20Off
+    echo $(wget -qO- http://$1/cm?cmnd=Power) > /tmp/$1
+    echo "$(date): $1 off" >> /tmp/PindaNetTimerDebug.txt
   fi
 }
 
+function thermostat {
+  temp=$(tail -1 $PresHumiTempfile)
+  temp=${temp%% C*}
+  # remove leading whitespace characters
+  temp="${temp#"${temp%%[![:space:]]*}"}"
+  if [ ! -f $thermostatkitchenfile ]; then # default
+    printf "%s\n" "${thermostatkitchendefault[@]}" > $thermostatkitchenfile
+    chown www-data:www-data $thermostatkitchenfile
+  fi
+  mapfile -t raw < $thermostatkitchenfile
+
+  IFS=$'\n' thermostatkitchen=($(sort <<<"${raw[*]}"))
+  unset IFS
+#  unset raw
+
+#  printf "%s\n" "${thermostatkitchen[@]}"
+#  echo $now
+
+  heating="off"
+  for thermostatitem in "${thermostatkitchen[@]}"; do
+    daytime=(${thermostatitem})
+    if [[ "${daytime[0]}" < "$now" ]] && [[ "${daytime[1]}" > "$now" ]]; then
+      heating="on"
+      break
+    fi
+  done
+  if [ "$heating" == "on" ]; then
+    if (( $(awk "BEGIN {print ($temp < ${daytime[2]} - $hysteresis)}") )); then
+      echo "Keukenverwarming inschakelen"
+      tasmota $KeukenZuidOn
+    elif (( $(awk "BEGIN {print ($temp > ${daytime[2]} + $hysteresis)}") )); then
+      echo "Keukenverwarming uitschakelen"
+      tasmota $KeukenZuidOff
+    fi
+  else
+    echo "Keuken niet verwarmen"
+    tasmota $KeukenZuidOff
+  fi
+
+  if [ ! -f $thermostatlivingfile ]; then # default
+    printf "%s\n" "${thermostatlivingdefault[@]}" > $thermostatlivingfile
+    chown www-data:www-data $thermostatlivingfile
+  fi
+  mapfile -t raw < $thermostatlivingfile
+
+  IFS=$'\n' thermostatliving=($(sort <<<"${raw[*]}"))
+  unset IFS
+#  unset raw
+
+#  printf "%s\n" "${thermostatliving[@]}"
+  echo $now
+
+  heating="off"
+  for thermostatitem in "${thermostatliving[@]}"; do
+    daytime=(${thermostatitem})
+    if [[ "${daytime[0]}" < "$now" ]] && [[ "${daytime[1]}" > "$now" ]]; then
+      heating="on"
+      break
+    fi
+  done
+  if [ "$heating" == "on" ]; then
+echo "Kamertemp: $temp °C"
+echo "Thermostaat temp: ${daytime[2]}"
+echo "Zuid aan bij" $(awk "BEGIN {print (${daytime[2]} - $hysteresis)}") "°C."
+echo "Noord aan bij" $(awk "BEGIN {print (${daytime[2]} - $hysteresis - $hysteresis * 2)}") "°C."
+echo "Noord uit bij" $(awk "BEGIN {print (${daytime[2]} + $hysteresis - $hysteresis * 2)}") "°C."
+echo "Zuid uit bij" $(awk "BEGIN {print (${daytime[2]} + $hysteresis)}") "°C."
+    if (( $(awk "BEGIN {print ($temp < ${daytime[2]} - $hysteresis)}") )); then
+      echo "Livingverwarming Zuid inschakelen"
+      sendRF $LivingZuidOn
+      if (( $(awk "BEGIN {print ($temp < ${daytime[2]} - $hysteresis - $hysteresis * 2)}") )); then
+        echo "Livingverwarming Noord inschakelen"
+        sendRF $LivingNoordOn
+      fi
+    elif (( $(awk "BEGIN {print ($temp > ${daytime[2]} + $hysteresis - $hysteresis * 2)}") )); then
+      echo "Livingverwarming Noord uitschakelen"
+      sendRF $LivingNoordOff
+      if (( $(awk "BEGIN {print ($temp > ${daytime[2]} + $hysteresis)}") )); then
+        echo "Livingverwarming Zuid uitschakelen"
+        sendRF $LivingZuidOff
+      fi
+    fi
+  else
+    echo "Living niet verwarmen"
+    sendRF $LivingZuidOff
+    sendRF $LivingNoordOff
+  fi
+}
+
+# Collect sensordata
 # BME280 I2C Temperature and Pressure Sensor
 # 3v3 - Vin
 # Gnd - Gnd
 # BCM 3 (SCL) - SCK (White)
 # BCM 2 (SDA) - SDI (Brown)
 # read pressure, humididy and temperature from sensor
-read_bme280 --i2c-address 0x77 > /var/www/html/data/PresHumiTemp
+read_bme280 --i2c-address 0x77 > $PresHumiTempfile
 
-#if [ $(cat /var/PindaNet/heating) == "on" ]; then
-#  heating on "manual"
-#  exit
-#elif [ $(cat /var/PindaNet/heating) == "off" ]; then
-#  heating off "manual"
-#  exit
-#fi
+# Calculate lux tls2591
+lux=$(python3 /var/www/html/tls2591.py | awk '{print $1}')
+echo $lux  > /var/www/html/data/luxtls
+if [ ! -f /var/www/html/data/luxmaxtls ]; then
+  echo 0 > /var/www/html/data/luxmaxtls
+fi
+luxmax=$(cat /var/www/html/data/luxmaxtls)
+if [ ! -f /var/www/html/data/luxmintls ]; then
+  echo 1000000 > /var/www/html/data/luxmintls
+fi
+luxmin=$(cat /var/www/html/data/luxmintls)
+if [ ${lux%.*} -eq ${luxmax%.*} ] && [ ${lux#*.} \> ${luxmax#*.} ] || [ ${lux%.*} -gt ${luxmax%.*} ]; then
+  luxmax=$lux
+fi
+if [ ${lux%.*} -eq ${luxmin%.*} ] && [ ${lux#*.} \< ${luxmin#*.} ] || [ ${lux%.*} -lt ${luxmin%.*} ]; then
+  luxmin=$lux
+fi
+echo $luxmax > /var/www/html/data/luxmaxtls
+echo $luxmin > /var/www/html/data/luxmintls
 
-#if [ ! -f /var/PindaNet/thermostat ]; then
-#  echo -n "0;09:30;20.00 0;21:25;-off- 1;09:30;20.00 1;21:25;-off- 2;09:30;20.00 2;21:25;-off- 3;09:30;20.00 3;21:25;-off- 4;09:30;20.00 4;21:25;-off- 5;09:30;20.00 5;21:25;-off- 6;09:30;20.00 6;21:25;-off-" > /var/PindaNet/thermostat
-#fi
-thermostat=`cat /var/www/html/data/thermostat`
+#echo "Omgevingslicht: $lux"
+#echo Maximaal gemeten omgevingslicht: $luxmax
+#echo Minimaal gemeten omgevingslicht: $luxmin
+rangelux=$(awk "BEGIN {printf \"%.2f\", $luxmax - $luxmin}")
+#echo Bereik gemeten omgevingslicht: $rangelux
+rellux=$(awk "BEGIN {printf \"%.2f\", ($lux - $luxmin) / $rangelux}")
+
+#rellux=$(awk "BEGIN {printf sqrt($rellux)}")
+
+# https://ledshield.wordpress.com/2012/11/13/led-brightness-to-your-eye-gamma-correction-no/
+correction=(65535 65508 65479 65451 65422 65394 65365 65337
+    65308 65280 65251 65223 65195 65166 65138 65109
+    65081 65052 65024 64995 64967 64938 64909 64878
+    64847 64815 64781 64747 64711 64675 64637 64599
+    64559 64518 64476 64433 64389 64344 64297 64249
+    64200 64150 64099 64046 63992 63937 63880 63822
+    63763 63702 63640 63577 63512 63446 63379 63310
+    63239 63167 63094 63019 62943 62865 62785 62704
+    62621 62537 62451 62364 62275 62184 62092 61998
+    61902 61804 61705 61604 61501 61397 61290 61182
+    61072 60961 60847 60732 60614 60495 60374 60251
+    60126 59999 59870 59739 59606 59471 59334 59195
+    59053 58910 58765 58618 58468 58316 58163 58007
+    57848 57688 57525 57361 57194 57024 56853 56679
+    56503 56324 56143 55960 55774 55586 55396 55203
+    55008 54810 54610 54408 54203 53995 53785 53572
+    53357 53140 52919 52696 52471 52243 52012 51778
+    51542 51304 51062 50818 50571 50321 50069 49813
+    49555 49295 49031 48764 48495 48223 47948 47670
+    47389 47105 46818 46529 46236 45940 45641 45340
+    45035 44727 44416 44102 43785 43465 43142 42815
+    42486 42153 41817 41478 41135 40790 40441 40089
+    39733 39375 39013 38647 38279 37907 37531 37153
+    36770 36385 35996 35603 35207 34808 34405 33999
+    33589 33175 32758 32338 31913 31486 31054 30619
+    30181 29738 29292 28843 28389 27932 27471 27007
+    26539 26066 25590 25111 24627 24140 23649 23153
+    22654 22152 21645 21134 20619 20101 19578 19051
+    18521 17986 17447 16905 16358 15807 15252 14693
+    14129 13562 12990 12415 11835 11251 10662 10070
+    9473 8872 8266 7657 7043 6424 5802 5175
+    4543 3908 3267 2623 1974 1320 662 0)
+correctionTableIndex=$(awk "BEGIN {printf \"%.0f\", 255 - ($rellux * 255)}")
+correctionTableValue=${correction[$correctionTableIndex]}
+correctionTableBrightness=$(awk "BEGIN {printf $correctionTableValue / ${correction[0]}}")
+#echo CorrectionTable Relatief omgevingslicht: $correctionTableBrightness
+rellux=$correctionTableBrightness
+
+#echo Relatief omgevingslicht: $rellux
+
+backlight=$(awk "BEGIN {printf \"%.0f\", 25 + $rellux * 230}")
+current=$(cat /sys/class/backlight/rpi_backlight/brightness)
+# Smooth backlight adjustment
+echo "Brightness from $current to $backlight"
+if [ $current -lt $backlight ]; then
+  for i in $(seq $current $backlight); do
+    echo $i > /sys/class/backlight/rpi_backlight/brightness
+  done
+else
+  for i in $(seq $current -1 $backlight); do
+    echo $i > /sys/class/backlight/rpi_backlight/brightness
+  done
+fi
+
+if [ ! -f $timerfile ]; then # default
+  raw[0]="0 07:20 12:00"
+  raw[1]="2 07:20 22:45"
+  raw[2]="4 07:20 22:45"
+  raw[3]="6 07:20 22:45"
+  raw[4]="1 07:20 22:45"
+  raw[5]="3 07:20 22:45"
+  raw[6]="5 07:20 22:45"
+#  raw[7]="0 16:30 17:00"
+  raw[8]="0 17:30 22:45"
+  printf "%s\n" "${raw[@]}" > $timerfile
+  chown www-data:www-data $timerfile
+fi
+mapfile -t raw < $timerfile
+
+IFS=$'\n' timer=($(sort <<<"${raw[*]}"))
+unset IFS
+unset raw
+
+state="sleep"
 weekday=$(date +%w)
 now=$(date +%H:%M)
-
-thermostatTemp=${thermostat: -5}
-
-data=${thermostat:0:13}
-thermostat=${thermostat: 14}
-while [ "$data" != "" ]; do
-  if [[ "${data:0:1}" > "$weekday" ]]; then
-    break
-  elif [[ "${data:0:1}" < "$weekday" ]]; then
-    thermostatTemp=${data:8:5}
-  elif [[ "${data:2:5}" < "$now" ]]; then
-    thermostatTemp=${data:8:5}
-  fi 
-  data=${thermostat:0:13}
-  thermostat=${thermostat: 14}
+for timeritem in "${timer[@]}"; do
+  daytime=(${timeritem})
+  if [ "${daytime[0]}" == "$weekday" ]; then
+    echo "$timeritem"
+    if [[ "${daytime[1]}" < "$now" ]] && [[ "${daytime[2]}" > "$now" ]]; then
+      state="awake"
+      break
+    fi
+  fi
 done
-
-temp=$(tail -1 /var/www/html/data/PresHumiTemp)
-temp=${temp%% C*}
-  # remove leading whitespace characters
-  temp="${temp#"${temp%%[![:space:]]*}"}"
-
-#echo "temp=$temp thermostatTemp=$thermostatTemp" >> /tmp/PindaNetDebug.txt
-hysteresis="0.1"
-if (( $(awk "BEGIN {print ($temp < $thermostatTemp - $hysteresis)}") )); then
-#  echo "temp=$temp thermostatTemp=$thermostatTemp Verwarming aan" #>> /tmp/PindaNetDebug.txt 
-  heating on "auto ($temp)"
-elif (( $(awk "BEGIN {print ($temp > $thermostatTemp + $hysteresis)}") )); then
-#  echo "temp=$temp thermostatTemp=$thermostatTemp Verwarming uit" #>> /tmp/PindaNetDebug.txt 
-  heating off "auto ($temp)"
-fi
-exit
-
-  # normalise floats to compare them
-  while [[ ${#temp} < ${#thermostatTemp} ]]; do
-    temp="0$temp"
-  done
-  while [[ ${#thermostatTemp} < ${#temp} ]]; do
-    thermostatTemp="0$thermostatTemp"
-  done
-
-#echo "$(date): weekday=$weekday now=$now temp=$temp thermostatTemp=$thermostatTemp" >> /tmp/PindaNetDebug.txt
-
-if [ "$thermostatTemp" == "-off-" ]; then
-  heating off "auto"
+echo $state
+if [ $state == "awake" ]; then
+  echo 0 > /sys/class/backlight/rpi_backlight/bl_power
+#  echo 255 > /sys/class/leds/led0/brightness
+#  echo 255 > /sys/class/leds/led1/brightness
 else
-#  thermostatTemp=$(php -r "echo number_format($thermostatTemp - 2, 2);")
-  # normalise floats to compare them
-#  while [[ ${#thermostatTemp} < ${#temp} ]]; do
-#    thermostatTemp="0$thermostatTemp"
-#  done
-#echo "temp=$temp thermostatTemp=$thermostatTemp" >> /tmp/PindaNetDebug.txt
-  if [[ "$temp" < "$thermostatTemp" ]]; then
-    heating on "auto ($temp)"
-  elif [[ "$temp" > "$thermostatTemp" ]]; then
-    heating off "auto ($temp)"
+  # deactivate backlight touchscreen
+  echo 1 > /sys/class/backlight/rpi_backlight/bl_power
+  # disable status led's
+  echo 0 > /sys/class/leds/led0/brightness
+  echo 0 > /sys/class/leds/led1/brightness
+  if (echo > /dev/tcp/rpiwall/22) >/dev/null 2>&1; then
+    # shutdown RPIWall
+    wget --post-data="command=halt" --quiet http://rpiwall/remote.php
+    sleep 30
+    # Power off RPIWall
+    python /home/pi/rfxcmd_gc-master/rfxcmd.py -d /dev/ttyUSB0 -s "0B 11 00 00 01 25 4A AE 0D 00 00 80"
   fi
 fi
+thermostat
