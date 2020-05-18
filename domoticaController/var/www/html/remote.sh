3b8604e1cba2f54a513cc3ab46734206238e61f8
Dany Pinoy <dany.pinoy@pindanet.be>
2020-05-18 18:54:47 +0200

    Create remote.sh

---
+++ domoticaController/var/www/html/remote.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#command=$(cat $1)
+command=$1
+#cat $1 > /root/remote.log
+echo $1 > /root/remote.log
+[[ -d /var/www/html/remote ]] || mkdir -p /var/www/html/remote
+if [ ${#command}  -lt 7 ]; then
+#  rm $1
+  case "$command" in
+#    halt)
+#      shutdown -h now
+#      ;;
+#    rsync)
+#      rsync -aAXv --delete --relative /home/dany/wall_install.sh /usr/sbin/background.sh /var/www/ pindabackup::backup/rpiwall > /root/rsync.log 2>&1
+#      mv /root/rsync.log /var/www/html/remote/rsync.log
+#      ;;
+    update)
+      apt-get clean -y > /root/update.log 2>&1
+      apt-get update -y >> /root/update.log 2>&1
+      apt-get upgrade -y >> /root/update.log 2>&1
+      mv /root/update.log /var/www/html/remote/update.log
+      ;;
+    clean)
+#      rm /var/www/html/remote/rsync.log
+      rm /var/www/html/remote/update.log
+      ;;
+  esac
+fi
