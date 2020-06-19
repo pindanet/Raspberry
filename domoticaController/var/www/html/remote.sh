577d59ea674266a7fc852896e2d70c4582908d96
Dany Pinoy <dany.pinoy@pindanet.be>
2020-06-19 13:56:12 +0200

    Update remote.sh

--- domoticaController/var/www/html/remote.sh
+++ domoticaController/var/www/html/remote.sh
@@ -10,10 +10,10 @@ if [ ${#command}  -lt 7 ]; then
 #    halt)
 #      shutdown -h now
 #      ;;
-#    rsync)
-#      rsync -aAXv --delete --relative /home/dany/wall_install.sh /usr/sbin/background.sh /var/www/ pindabackup::backup/rpiwall > /root/rsync.log 2>&1
-#      mv /root/rsync.log /var/www/html/remote/rsync.log
-#      ;;
+    rsync)
+      rsync -aAXv --delete --relative /usr/sbin/background.sh /etc/systemd/system/background.service /usr/sbin/PindaNetDomo.sh /etc/systemd/system/PindaNetDomo.service /var/www/ pindabackup::backup/pindadomo > /root/rsync.log 2>&1
+      mv /root/rsync.log /var/www/html/remote/rsync.log
+      ;;
     update)
       apt-get clean -y > /root/update.log 2>&1
       apt-get update -y >> /root/update.log 2>&1
@@ -21,7 +21,7 @@ if [ ${#command}  -lt 7 ]; then
       mv /root/update.log /var/www/html/remote/update.log
       ;;
     clean)
-#      rm /var/www/html/remote/rsync.log
+      rm /var/www/html/remote/rsync.log
       rm /var/www/html/remote/update.log
       ;;
   esac
