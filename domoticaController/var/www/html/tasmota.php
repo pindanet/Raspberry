a287c1438c448b5fa72975a1590755974b5baec5
Dany Pinoy <dany.pinoy@pindanet.be>
2020-09-25 19:33:30 +0200

    Create tasmota.php

---
+++ domoticaController/var/www/html/tasmota.php
@@ -0,0 +1,12 @@
+<?php
+
+// Use terminal arg as POST en GET arg, example: php tasmota.php dev=e7b609-5641 cmd=Power%20toggle
+if (!isset($_SERVER["HTTP_HOST"])) {
+  parse_str(implode('&', array_slice($argv, 1)), $_GET);
+  parse_str(implode('&', array_slice($argv, 1)), $_POST);
+}
+
+$dev = htmlspecialchars($_POST["dev"]);
+$cmd = htmlspecialchars($_POST["cmd"]);
+
+exec("wget -qO- \"http://tasmota_$dev/cm?cmnd=$cmd\"");
