a4e95f4132e56e3357c9c6f20091ec50b4afb4ae
Dany Pinoy <dany.pinoy@pindanet.be>
2021-04-10 18:44:58 +0200

    Update PindaNetDining.sh

--- dining/usr/sbin/PindaNetDining.sh
+++ dining/usr/sbin/PindaNetDining.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-temp=$(python /home/*/ds18b20.py)
-LC_ALL=C printf "%.1f °C" "$temp" > /home/*/temp.txt
+while true
+do
+  starttime=$(date +"%s") # complete cycle: 1 minute
+
+  temp=$(python /home/*/ds18b20.py)
+  LC_ALL=C printf "%.1f °C" "$temp" > /home/*/temp.txt
+
+  sleepSec=$((60 - ($(date +"%s") - starttime)))
+  sleep $sleepSec
+done
