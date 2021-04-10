13effd4aca12b0554db7ac85abe2d351ea671f35
Dany Pinoy <dany.pinoy@pindanet.be>
2021-04-10 15:48:13 +0200

    Update PindaNetDining.sh

--- dining/usr/sbin/PindaNetDining.sh
+++ dining/usr/sbin/PindaNetDining.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-temp=$(python /home/dany/ds18b20.py)
-LC_ALL=C printf "%.1f °C" "$temp" > /home/dany/temp.txt
+temp=$(python /home/*/ds18b20.py)
+LC_ALL=C printf "%.1f °C" "$temp" > /home/*/temp.txt
