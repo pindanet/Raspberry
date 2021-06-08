19c5b4a24a33bd80c89e9350b6eb40457ed441ba
Dany Pinoy <dany.pinoy@pindanet.be>
2021-06-08 18:56:08 +0200

    Update alarmclock_install.sh

--- alarmclock_install.sh
+++ alarmclock_install.sh
@@ -164,10 +164,11 @@ EOF
   sudo systemctl daemon-reload
   sudo systemctl enable PindaNetSleep.service
 
-wget https://orangefreesounds.com/wp-content/uploads/2019/01/Old-alarm-clock-sound.zip
-unzip Old-alarm-clock-sound.zip
-sudo cp Old-alarm-clock-sound.mp3 /var/www/html/data/
+#wget https://orangefreesounds.com/wp-content/uploads/2019/01/Old-alarm-clock-sound.zip
+#unzip Old-alarm-clock-sound.zip
+#sudo cp Old-alarm-clock-sound.mp3 /var/www/html/data/
 
+sudo wget -O /var/www/html/data/bintro.mp3 https://raw.githubusercontent.com/pindanet/Raspberry/master/alarmclock/var/www/html/data/bintro.mp3
 sudo wget -O /var/www/html/alarmnow.sh https://raw.githubusercontent.com/pindanet/Raspberry/master/alarmclock/var/www/html/alarmnow.sh
 sudo chmod +x /var/www/html/alarmnow.sh
   
