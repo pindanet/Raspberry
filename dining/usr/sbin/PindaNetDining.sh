e45a15a0fd9527a7203c4062b56b924635203e3a
Dany Pinoy <dany.pinoy@pindanet.be>
2021-07-05 20:12:36 +0200

    Update PindaNetDining.sh

--- dining/usr/sbin/PindaNetDining.sh
+++ dining/usr/sbin/PindaNetDining.sh
@@ -6,6 +6,10 @@ do
   temp=$(python /home/*/ds18b20.py)
   LC_ALL=C printf "%.1f °C" "$temp" > /home/*/temp.txt
 
+  sudo pkill -9 pngview
+  convert -size 1920x70 xc:none -font Bookman-DemiItalic -pointsize 32 -fill white -stroke black -gravity center -draw "text 0,0 '$(date +"%A, %e %B %Y   %k:%M")   $(cat /home/*/temp.txt)'" /home/*/image.png
+  /home/*/raspidmx-master/pngview/pngview -b 0 -l 3 -y 1130 /home/*/image.png &
+
   sleepSec=$((60 - ($(date +"%s") - starttime)))
   sleep $sleepSec
 done
