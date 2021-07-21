dfd52080d44df92eed9cf3a71583b859a0af97ca
Dany Pinoy <dany.pinoy@pindanet.be>
2021-07-21 20:06:19 +0200

    Update PindaNetDining.sh

--- dining/usr/sbin/PindaNetDining.sh
+++ dining/usr/sbin/PindaNetDining.sh
@@ -1,5 +1,36 @@
 #!/bin/bash
+function relayGPIO () {
+  _r1_pin=23
+  # Activate GPIO Relay
+  if [ ! -d "/sys/class/gpio/gpio$_r1_pin" ]; then
+    #   Exports pin to userspace
+    echo $_r1_pin > /sys/class/gpio/export
+    # Sets pin as an output
+    echo "out" > /sys/class/gpio/gpio$_r1_pin/direction
+  fi
+
+  if [ ! -f /tmp/$1 ]; then # initialize
+    echo '{"POWER":"ON"}' > /tmp/$1
+    echo "$(date -u +%s),off" >> /var/www/html/data/$1.log
+  fi
+  if [ $2 == "on" ] && [ "$(cat /tmp/$1)" == '{"POWER":"OFF"}' ]; then
+    echo 0 > /sys/class/gpio/gpio$_r1_pin/value # Power on
+    echo '{"POWER":"ON"}' > /tmp/$1
+    echo "$(date -u +%s),$2" >> /var/www/html/data/$1.log
+  elif [ $2 == "off" ] && [ "$(cat /tmp/$1)" == '{"POWER":"ON"}' ]; then
+    echo 1 > /sys/class/gpio/gpio$_r1_pin/value # Power off
+    echo '{"POWER":"OFF"}' > /tmp/$1
+    echo "$(date -u +%s),$2" >> /var/www/html/data/$1.log
+  elif [ "$(cat /tmp/$1)" != '{"POWER":"OFF"}' ] && [ "$(cat /tmp/$1)" != '{"POWER":"ON"}' ]; then
+    echo '{"POWER":"ON"}' > /tmp/$1
+    echo "$(date): Relay error. Heating $1" >> /tmp/PindaNetDebug.txt
+  fi
+}
 function tasmota () {
+  if [[ $1 == *"relayGPIO"* ]]; then
+    relayGPIO $1 $2
+    return
+  fi
   if [ ! -f /tmp/$1 ]; then # initialize
     echo $(wget -qO- http://$1/cm?cmnd=Power) > /tmp/$1
     two=$(wget -qO- http://$1/cm?cmnd=Power | awk -F"\"" '{print $4}')
@@ -23,78 +54,6 @@ function tasmota () {
 }
 
 . /var/www/html/data/thermostat
-
-# test
-unset domoDefault
-domoDefault+=("07:30 08:30 dining")	# breakfast
-domoDefault+=("11:00 12:00 dining kitchen")	# prepare meal
-domoDefault+=("12:00 12:15 dining")	# lunch
-domoDefault+=("12:15 13:00 kitchen")	# dishes
-domoDefault+=("13:00 13:25 dining")	# coffee break
-domoDefault+=("17:00 17:15 dining")	# dinner
-domoDefault+=("22:25 22:50 kitchen")	# bedtime
-
-# Events
-unset domoevent
-domoevent+=("2021-08-18 08:00 12:00 -r 14 -a 07:00") # Laura om de 14 dagen
-domoevent+=("2021-06-22 08:00 09:00 -a 06:30") # Tandarts Liesbeth Donck, opstaan om 06:30
-
-domoevent+=("2021-07-13 10:00 11:00") # Test
-domoevent+=("2021-06-29 09:00 10:00 -r 7") # Repeat test
-domoevent+=("2021-06-29 08:00 09:00 -r 7 -a 07:00") # Repeat && alarm test
-
-# Get today's events
-unset todayevent
-for domoeventitem in "${domoevent[@]}"; do
-  event=(${domoeventitem})
-  recevent=$(date -u --date "${event[0]}" +%s)
-  todaySec=$(date -u +%s)
-  today=$((todaySec - (todaySec % 86400)))
-  if [[ $domoeventitem == *"-r "* ]]; then # recurrent event
-    for (( i=0; i<=${#event[@]}; i++ )); do
-      if [[ ${event[$i]} == "-r" ]]; then
-        period=${event[$((i + 1))]} # every x days
-        timebetween=$((period * 86400))
-        while  [ $recevent -lt $today ]; do
-          recevent=$((recevent + timebetween))
-        done
-        break
-      fi
-    done
-  fi
-  if [ $today == $recevent ]; then
-    todayevent+=("${event[1]} ${event[2]}")
-  fi
-done
-IFS=$'\n' sorted=($(sort <<<"${todayevent[*]}"))
-unset IFS
-echo ${sorted[@]}
-# ToDo
-# Get beginning of day: domoOn
-
-room="dining"
-# Day starts with breakfast
-breakfastSec=$(date -d $domoOn +%s)
-breakfast=(${domoDefault[0]})
-domoDefault[0]="$(date -d @$((breakfastSec + 15 * 60)) +%H:%M) $(date -d @$((breakfastSec + 60 * 60)) +%H:%M)"
-for (( i=2; i<=${#breakfast[@]}; i++ )); do
-  domoDefault[0]="${domoDefault[0]} ${breakfast[$i]}"
-done
-
-unset domo
-for domoitem in "${domoDefault[@]}"; do
-  if [[ $domoitem == *"$room"* ]]; then
-    dayitem=(${domoitem})
-    domo+=("${dayitem[0]} ${dayitem[1]}")
-#    echo $domoitem
-  fi
-done
-
-for domoitem in "${domo[@]}"; do
-  echo $domoitem
-done
-# end test
-
 # Calculated configs
 # domoOn
 today=$(date +%u)
@@ -120,7 +79,8 @@ done
 echo $domoOn > /var/www/html/data/domoOn
 
 # Lights on in the morning 
-wget -qO- http://$diningLight/cm?cmnd=Power%20On
+#wget -qO- http://$diningLight/cm?cmnd=Power%20On
+raspi-gpio set $diningLight op dl
 
 # Lights out in the morning
 # From UTC
@@ -135,9 +95,11 @@ sunriseLocalSec=$((sunriseSec + localToUTC * 3600))
 # to Local
 sunrise=$(date -d @$sunriseLocalSec +"%H:%M")
 if [[ $lightsOut > $sunrise ]]; then # sun shines
-  echo "wget -qO- http://$diningLight/cm?cmnd=Power%20Off" | at $lightsOut
+  echo "raspi-gpio set $diningLight op dh" | at $lightsOut
+#  echo "wget -qO- http://$diningLight/cm?cmnd=Power%20Off" | at $lightsOut
 else # still dark
-  echo "wget -qO- http://$diningLight/cm?cmnd=Power%20Off" | at $sunrise
+  echo "raspi-gpio set $diningLight op dh" | at $sunrise
+#  echo "wget -qO- http://$diningLight/cm?cmnd=Power%20Off" | at $sunrise
 fi
 
 # Lights on in the evening
@@ -148,12 +110,15 @@ sunsetLocalSec=$((sunsetSec + localToUTC * 3600))
 # to Local
 sunset=$(date -d @$sunsetLocalSec +"%H:%M")
 if [[ $eveningShutterDown > $sunset ]]; then # already dark
-  echo "wget -qO- http://$diningLight/cm?cmnd=Power%20On" | at $sunset
+  echo "raspi-gpio set $diningLight op dl" | at $sunset
+#  echo "wget -qO- http://$diningLight/cm?cmnd=Power%20On" | at $sunset
 else # still daylight
-  echo "wget -qO- http://$diningLight/cm?cmnd=Power%20On" | at $eveningShutterDown
+  echo "raspi-gpio set $diningLight op dl" | at $eveningShutterDown
+#  echo "wget -qO- http://$diningLight/cm?cmnd=Power%20On" | at $eveningShutterDown
 fi
 # All lights out
-echo "wget -qO- http://$diningLight/cm?cmnd=Power%20Off" | at $lightevening
+echo "raspi-gpio set $diningLight op dh" | at $lightevening
+#echo "wget -qO- http://$diningLight/cm?cmnd=Power%20Off" | at $lightevening
 
 while true
 do
