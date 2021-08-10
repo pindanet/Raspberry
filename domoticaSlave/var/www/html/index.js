78640bcd84b7beacaf8ceb75a8f44345161e0236
Dany Pinoy <dany.pinoy@pindanet.be>
2021-08-10 11:10:16 +0200

    Update index.js

--- domoticaSlave/var/www/html/index.js
+++ domoticaSlave/var/www/html/index.js
@@ -1,6 +1,38 @@
+var lightSwitch="tasmota_15dd89-7561";
+
 var startTimer;
 var dayNames = new Array("Zondag","Maandag","Dinsdag","Woensdag","Donderdag","Vrijdag","Zaterdag");
 var monthNames = new Array("januari","februari","maart","april","mei","juni","juli","augustus","september","oktober","november","december");
+
+function lightstatus() {
+  var xhr = new XMLHttpRequest();
+  xhr.open('POST', "data/light-bulb", true);
+  xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  xhr.onload = function(e) { 
+    if (this.status == 200) {
+      if (this.responseText.includes("ON")) {
+        document.getElementById("light").src = "emoji/light-bulb-on.svg";
+      } else {
+        document.getElementById("light").src = "emoji/light-bulb-off.svg";
+      }
+    }
+  };
+  xhr.send("dev=" + lightSwitch + "&cmd=Power%20toggle");
+}
+
+function lights(event) {
+  var xhr = new XMLHttpRequest();
+  xhr.open('POST', "tasmota.php", true);
+  xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  xhr.onload = function(e) {
+    if (this.status == 200) {
+      lightstatus();
+    }
+  };
+  xhr.send("dev=" + lightSwitch + "&cmd=Power%20toggle");
+
+  event.stopPropagation();
+}
 
 var roomTemp="20.0 °C";
 function getRoomTemp() {
@@ -34,6 +66,7 @@ function startTime() {
   document.getElementById('clock').innerHTML = h + ":" + m + ":" + s;
 
   getRoomTemp();
+  lightstatus();
 
   startTimer = setTimeout(startTime, 1000); // elke seconde
 }
