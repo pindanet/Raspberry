a3e4008b8592386d2b91c4a9af36aa1bd395bf8d
Dany Pinoy <dany.pinoy@pindanet.be>
2021-10-03 18:52:49 +0200

    Create ssh.php

---
+++ domoticaController/var/www/html/ssh.php
@@ -0,0 +1,21 @@
+<?php
+//ini_set('display_errors', 1);
+//ini_set('display_startup_errors', 1);
+//error_reporting(E_ALL);
+//Post data als argumenten bij php cli
+
+// php ssh.php command="uname -a"
+//if (!isset($_SERVER["HTTP_HOST"])) {
+//  parse_str($argv[1], $_GET);
+//  parse_str($argv[1], $_POST);
+//}
+
+$command = htmlspecialchars($_POST["command"]);
+$host = htmlspecialchars($_POST["host"]);
+exec("ls /home/", $users);
+exec("ssh -i data/id_rsa -o StrictHostKeyChecking=no  -o 'UserKnownHostsFile /dev/null' " . $users[0] . "@" . $host . " " . $command, $output, $return);
+
+foreach ($output as $line) {
+  echo "$line\n";
+}
+?>
