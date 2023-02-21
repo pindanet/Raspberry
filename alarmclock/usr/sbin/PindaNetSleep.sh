d76486413e9a133f0fc6a515d4fc3fea8d1ebce9
Dany Pinoy <dany.pinoy@pindanet.be>
2023-02-21 18:07:18 +0100

    Update PindaNetSleep.sh

--- alarmclock/usr/sbin/PindaNetSleep.sh
+++ alarmclock/usr/sbin/PindaNetSleep.sh
@@ -10,6 +10,32 @@ echo 0 > /sys/class/leds/led1/brightness
 sleep 1
 echo 0 > /sys/class/leds/led1/brightness
 echo 0 > /sys/class/leds/led0/brightness
+# Better add to /boot/config.txt
+# The Ethernet LEDs will take somewhere between 5-20minutes to completely turn off, so don’t panic if they are on initially.
+
+## RPI3B
+## Turn off Power LED
+#dtparam=pwr_led_trigger=default-on
+#dtparam=pwr_led_activelow=off
+# Turn off Activity LED
+#dtparam=act_led_trigger=none
+#dtparam=act_led_activelow=off
+## Turn off Ethernet ACT LED
+#dtparam=eth_led0=14
+## Turn off Ethernet LNK LED
+#dtparam=eth_led1=14
+
+## RPI4B
+## Turn off Power LED
+#dtparam=pwr_led_trigger=default-on
+#dtparam=pwr_led_activelow=off
+# Turn off Activity LED
+#dtparam=act_led_trigger=none
+#dtparam=act_led_activelow=off
+## Turn off Ethernet ACT LED
+#dtparam=eth_led0=4
+# Turn off Ethernet LNK LED
+# dtparam=eth_led1=4
 
 _button_pin=5
 
