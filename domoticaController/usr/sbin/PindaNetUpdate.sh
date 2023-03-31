d1771ee0561693a3a94e3d465f772c9455b601fe
Dany Pinoy <dany.pinoy@pindanet.be>
2023-03-31 16:37:00 +0200

    Update PindaNetUpdate.sh

--- domoticaController/usr/sbin/PindaNetUpdate.sh
+++ domoticaController/usr/sbin/PindaNetUpdate.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-if [ ! -f "/tmp/thermostatManual" ]; then
-  apt-get clean
-  apt-get update
-  apt-get upgrade -y
-  shutdown -r now
+if [ -f "/tmp/thermostatManual" ]; then
+  mv /tmp/thermostatManual /root/thermostatManual
 fi
+apt-get clean
+apt-get update
+apt-get upgrade -y
+shutdown -r now
