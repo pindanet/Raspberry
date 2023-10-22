bf7f9da8440605c157375de35c711d954c2ae6e7
Dany Pinoy <dany.pinoy@pindanet.be>
2023-10-22 17:51:48 +0200

    Update playRadio.sh

--- alarmclock/var/www/html/playRadio.sh
+++ alarmclock/var/www/html/playRadio.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # $1 = URL, $2 = volume
-interval=$(timeout 2 curl -H "Icy-MetaData:1" --silent -L "$1" 2>&1 | stdbuf -oL grep -aob "StreamTitle" | head -n1 | sed 's@^[^0-9]*\([0-9]\+\).*@\1@')
-interval=$((interval - 1))
+#interval=$(timeout 2 curl -H "Icy-MetaData:1" --silent -L "$1" 2>&1 | stdbuf -oL grep -aob "StreamTitle" | head -n1 | sed 's@^[^0-9]*\([0-9]\+\).*@\1@')
+#interval=$((interval - 1))
+interval="8192"
 curl -H "Icy-MetaData:1" --silent -L "$1" 2>&1 | mpg123 --icy-interval $interval -f -$2 -
