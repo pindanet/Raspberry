0ddd33b16b50ec22a136a0ba09f6503d08729415
Dany Pinoy <dany.pinoy@pindanet.be>
2023-11-12 18:32:55 +0100

    Update tasmota.sh

--- domoticaController/var/www/html/tasmota.sh
+++ domoticaController/var/www/html/tasmota.sh
@@ -1,26 +1,26 @@
-declare -A status=()   
+declare -A -l status=()
 function tasmota () { # power name
   power=$1
   name=$2
   url=${IPs["$name"]}
   watt=${Watts["$name"]}
-#  echo "Power: $power, Name: $name, Url: $url, Watt: $watt"
+  declare -l cmnd=${Cmnds["$name"]}
+#  echo "Power: $power, Name: $name, Url: $url, Watt: $watt, Cmnd: $cmnd"
   if [ -z ${status["$name"]} ]; then # initialize
-    status["$name"]=$(wget -qO- http://$url/cm?cmnd=Power)
+    status["$name"]=$(wget -qO- http://$url/cm?cmnd=$cmnd)
     two=$(echo ${status["$name"]} | awk -F"\"" '{print $4}')
-    twolower=${two,,}
-    if [ $twolower == "on" ] || [ $twolower == "off" ]; then
+    if [ $two == "on" ] || [ $two == "off" ]; then
       echo "$(date),$twolower,$watt" >> /var/www/html/data/$name.$logExt
     fi
   fi
-  if [ $power == "on" ] && [ "${status["$name"]}" == '{"POWER":"OFF"}' ]; then
-    status["$name"]=$(wget -qO- http://$url/cm?cmnd=Power%20On)
+  if [ $power == "on" ] && [ "${status["$name"]}" == '{"'$cmnd'":"off"}' ]; then
+    status["$name"]=$(wget -qO- http://$url/cm?cmnd=$cmnd%20On)
     echo "$(date),$power,$watt" >> /var/www/html/data/$name.$logExt
-  elif [ $power == "off" ] && [ "${status["$name"]}" == '{"POWER":"ON"}' ]; then
-    status["$name"]=$(wget -qO- http://$url/cm?cmnd=Power%20Off)
+  elif [ $power == "off" ] && [ "${status["$name"]}" == '{"'$cmnd'":"on"}' ]; then
+    status["$name"]=$(wget -qO- http://$url/cm?cmnd=$cmnd%20Off)
     echo "$(date),$power,$watt" >> /var/www/html/data/$name.$logExt
-  elif [ "${status["$name"]}" != '{"POWER":"OFF"}' ] && [ "${status["$name"]}" != '{"POWER":"ON"}' ]; then
-    status["$name"]=$(wget -qO- http://$url/cm?cmnd=Power)
+  elif [ "${status["$name"]}" != '{"'$cmnd'":"off"}' ] && [ "${status["$name"]}" != '{"'$cmnd'":"on"}' ]; then
+    status["$name"]=$(wget -qO- http://$url/cm?cmnd=$cmnd)
     echo "$(date): Communication error. Tasmota $name" >> /tmp/PindaNetDebug.txt
   fi
 }
