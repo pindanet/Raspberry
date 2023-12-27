8d0c72d727fc8c2b82df4a19119256d4c4cea21c
Dany Pinoy <dany.pinoy@pindanet.be>
2023-12-27 17:44:20 +0100

    Update staand.js

--- domoticaController/var/www/html/staand.js
+++ domoticaController/var/www/html/staand.js
@@ -55,19 +55,34 @@ var conf = {
 };
 console.log(conf);
 
+// define a function that converts a string to hex
+const stringToHex = (str) => {
+  let hex = '';
+  for (let i = 0; i < str.length; i++) {
+    const charCode = str.charCodeAt(i);
+    const hexValue = charCode.toString(16);
+
+    // Pad with zeros to ensure two-digit representation
+    hex += hexValue.padStart(2, '0');
+  }
+  return hex;
+};
+
 function tasmotaLedFadeInOut() {
-  for(let i = 4; i < 256; i++) {
-console.log(i.toString(16).padStart(2, '0')+i.toString(16).padStart(2, '0')+i.toString(16).padStart(2, '0'));
+// php /var/www/html/cli.php cmd=wget params=-qO-\ http://192.168.129.41/cm?cmnd=Power3
+//  for(let i = 4; i < 256; i++) {
+//console.log(i.toString(16).padStart(2, '0')+i.toString(16).padStart(2, '0')+i.toString(16).padStart(2, '0'));
     var xhr = new XMLHttpRequest();
-    xhr.open('GET', "http://192.168.129.41/cm", true);
-//    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhr.open('POST', "cli.php", true);
+    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
     xhr.onload = function(e) {
       if (this.status == 200) {
- console.log(this.responseText);
+        const output = JSON.parse(this.responseText);
+console.log(output);
       }
     };
-    xhr.send("cmnd=led");
-  }
+    xhr.send("cmd=wget&params="+stringToHex("-qO- http://192.168.129.41/cm?cmnd=Power3"));
+//  }
 }
 //tasmotaLedFadeInOut();
 var available = {};
