6ee7095689c21c5614c08ec6fc2ebcc0c362adbb
Dany Pinoy <dany.pinoy@pindanet.be>
2024-02-14 19:10:34 +0100

    Update thermostat.js

--- domoticaController/var/www/html/thermostat.js
+++ domoticaController/var/www/html/thermostat.js
@@ -1,3 +1,6 @@
+// ToDo
+// Manual temp
+
 // Configuration
 var conf = {
   available: [
@@ -59,10 +62,82 @@ var conf = {
       Cmnd: "Power2"
     }
   ],
+  Living: {
+      id: "living",
+      htmlElementId: "clockmonthday",
+      heaterColor: "",
+      temp: 20.0,
+      tempOffset: "0",
+      heater: [
+        {
+          name: "Schilderij",
+          color: "Yellow",
+          IP: "192.168.129.12",
+          Watt: "650"
+        },
+        {
+          name: "Computertafel",
+          color: "Orange",
+          IP: "192.168.129.0",
+          Watt: "300"
+        },
+        {
+          name: "Canyon",
+          color: "Red",
+          IP: "192.168.129.4",
+          Watt: "650"
+        }
+      ],
+      thermostat: [
+        {
+          begin: "07:30",
+          end: "13:30",
+          temp: "tempAux"
+	},
+        {
+          begin: "13:30",
+          end: "17:00",
+          temp: "tempComfort"
+	},
+        {
+          begin: "17:00",
+          end: "17:30",
+          temp: "tempAux"
+	},
+        {
+          begin: "17:30",
+          end: "22:50",
+          temp: "tempComfort"
+	}
+      ]
+  },
   Dining: {
       id: "dining",
+      htmlElementId: "clockmonth",
+      heaterColor: "",
+      temp: 20.0,
       tempOffset: "0",
       subtitleColor: "white",
+      heater: [
+        {
+          name: "Zonsondergang",
+          color: "Yellow",
+          IP: "192.168.129.20",
+          Watt: "650"
+        },
+        {
+          name: "Tafel",
+          color: "Orange",
+          IP: "192.168.129.5",
+          Watt: "300"
+        },
+        {
+          name: "Eekhoorn",
+          color: "Red",
+          IP: "192.168.129.3",
+          Watt: "650"
+        }
+      ],
       thermostat: [
         {
           begin: "07:30",
@@ -103,7 +178,7 @@ var conf = {
           begin: "17:30",
           end: "22:30",
           temp: "tempAux"
-	},
+	}
       ]
   },
   event: [
@@ -122,13 +197,13 @@ var conf = {
     },
     {
       repeat: 1,
-      begindate: "2024-02-13",
-      begin: "14:45",
-      enddate: "2024-02-13",
+      begindate: "2024-02-16",
+      begin: "15:30",
+      enddate: "2024-02-16",
       end: "bedTime",
       temp: {
         living: "tempAux",
-        dining: "tempComfort",
+        dining: "tempOff",
         kitchen: "tempOff"
       },
       comment: "Test"
@@ -153,10 +228,29 @@ var conf = {
 //console.log(conf.Dining.event[0]);
 //console.log(JSON.stringify(conf));
 sendConf(conf);
-function tempAdjustment(room, temp) {
+
+function getDiningTemp() {
+  var xhr = new XMLHttpRequest();
+  xhr.open('POST', "ssh.php", true);
+  xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  xhr.onload = function() {
+    if (this.readyState === 4) {
+      var diningTemp = parseFloat(this.responseText).toFixed(1);
+      if (!isNaN(diningTemp)) { // change with valid temp
+        document.getElementById("diningRoomTemp").innerHTML = diningTemp + " °C";
+      }
+    }
+  };
+  xhr.send('host=pindadining&command=cat /home/dany/temp.txt');
+}
+//var heaterColor = "";
+function tempAdjustment(room) {
+//room.temp = 16.9;
+  if (isNaN(room.temp)) {
+    return;
+  }
   var today = new Date();
   var now = today.getTime();
-  var heatingRoom = "off";
   var tempWanted = conf.tempOff;
   for (let i = 0; i < room.thermostat.length; i++) {
     var beginTime = room.thermostat[i].begin.split(':');
@@ -172,7 +266,6 @@ function tempAdjustment(room, temp) {
     endDate.setSeconds(0);
     var end = endDate.getTime();
     if (begin <= now && end > now) {
-      heatingRoom = "on";
       tempWanted = conf[room.thermostat[i].temp];
       break;
     }
@@ -189,7 +282,7 @@ function tempAdjustment(room, temp) {
     var begin = beginDate.getTime();
     if (conf.event[i].repeat > 0) { // repeating event
       while (begin < dateOnly) {
-        begin += 86400000;
+        begin += 86400000 * conf.event[i].repeat;
       }
     }
     if (begin == dateOnly) {
@@ -211,10 +304,10 @@ function tempAdjustment(room, temp) {
       var beginTime = conf.event[i].begin.split(':');
       beginDate.setHours(beginTime[0]);
       beginDate.setMinutes(beginTime[1]);
+      begin = beginDate.getTime();
 console.log("Event on " + beginDate.toString());
 console.log("Until " + endDate.toString());
       if (begin <= now && end > now) {
-        heatingRoom = "on";
         tempWanted = conf[conf.event[i].temp[room.id]];
 console.log("Temp wanted: " + tempWanted);
         break;
@@ -222,26 +315,55 @@ console.log("Temp wanted: " + tempWanted);
     }
   }
 
-console.log(beginDate.toString(), endDate.toString(), tempWanted);
+// ToDo
+// Manual temp
 
-  switch (tempWanted) {
-    case conf.tempOff:
-      document.getElementById("clockmonth").style.color = "blue";
-      break;
-    case conf.tempAux:
-      document.getElementById("clockmonth").style.color = "orange";
-      break;
-    case conf.tempComfort:
-      document.getElementById("clockmonth").style.color = "red";
-      break;
-    default:
-      document.getElementById("clockmonth").style.color = "";
-      break;
+  if (tempWanted == conf.tempOff) {
+    var nightTime = today.getHours().toString().padStart(2, '0') + ":" + today.getMinutes().toString().padStart(2, '0');
+    if (conf.tempNightTime > nightTime) {
+      tempWanted = conf.tempNight;
+    }
   }
+
+  for (let i = 0; i < room.heater.length; i++) {
+    var tempHysteresis = tempWanted - conf.hysteresis - conf.hysteresis * (2 * i);
+console.log(room.heater[i].name + " switch on at " + tempHysteresis.toFixed(2) + " °C");
+    if (room.temp < tempHysteresis) {
+//      tasmota room.heater[i]("on");
+console.log(room.heater[i].name + " switched on at " + room.temp + " °C");
+      room.heaterColor = room.heater[i].color;
+    }
+  }
+  for (let i = room.heater.length - 1; i <= 0; i--) {
+    tempHysteresis = tempWanted + conf.hysteresis - conf.hysteresis * (2 * i);
+console.log(room.heater[i].name + " switch off at " + tempHysteresis.toFixed(2) + " °C");
+    if (room.temp > tempHysteresis) {
+//      tasmota room.heater[i]("off");
+console.log(room.heater[i].name + " switched off at " + room.temp + " °C");
+      if (i == 0) {
+        room.heaterColor = "";
+      } else {
+        room.heaterColor = room.heater[i - 1].color;
+      }
+    }
+  }
+
+console.log(room.temp, tempWanted, room.heaterColor);
+
+  document.getElementById(room.htmlElementId).style.color = room.heaterColor;
 }
 function thermostat() {
-  var temp;
   var thermostatdefault;
+  conf.Living.temp = parseFloat(document.getElementById("minitemp").innerHTML);
+  tempAdjustment(conf.Living);
+//  getLivingTemp();
+  conf.Dining.temp = parseFloat(document.getElementById("diningRoomTemp").innerHTML);
+  tempAdjustment(conf.Dining);
+  getDiningTemp();
+//  tempAdjustment(conf.Kitchen, temp);
+//  getKitchenTemp();
+  setTimeout(thermostat, 60000); // Every minute
+
 // Get temp Living
 //  var xhr = new XMLHttpRequest();
 //  xhr.open('POST', "data/PresHumiTemp", true);
@@ -255,20 +377,19 @@ function thermostat() {
 //  };
 //  xhr.send();
 // Get temp Dining
-  var xhr = new XMLHttpRequest();
-  xhr.open('POST', "ssh.php", true);
-  xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-  xhr.onload = function() {
-    if (this.readyState === 4) {
-      temp = parseFloat(this.responseText).toFixed(2);
-      if (!isNaN(temp)) { // If valid temp
-        tempAdjustment(conf.Dining, temp);
-      }
-    }
-  };
-  xhr.send('host=pindadining&command=cat /home/dany/temp.txt');
+//  var xhr = new XMLHttpRequest();
+//  xhr.open('POST', "ssh.php", true);
+//  xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+//  xhr.onload = function() {
+//    if (this.readyState === 4) {
+//      temp = parseFloat(this.responseText).toFixed(2);
+//      if (!isNaN(temp)) { // If valid temp
+//        tempAdjustment(conf.Dining, temp);
+//      }
+//    }
+//  };
+//  xhr.send('host=pindadining&command=cat /home/dany/temp.txt');
 // Get temp Kitchen
-  setTimeout(thermostat, 60000); // Every minute
 }
 function sendConf(obj) {
     var xhr = new XMLHttpRequest();
@@ -282,7 +403,5 @@ function sendConf(obj) {
     };
     xhr.send(JSON.stringify(obj));
 }
-setTimeout(thermostat, 6000);
+setTimeout(thermostat, 60000);
 // Every minute// End of Thermostat
-
-
