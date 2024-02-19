30d2de433b56446c6b216c60a0619776461c71ea
Dany Pinoy <dany.pinoy@pindanet.be>
2024-02-19 19:04:13 +0100

    Update domoticaController_install.sh

--- domoticaController_install.sh
+++ domoticaController_install.sh
@@ -259,6 +259,7 @@ EOF
   sudo wget -O /var/www/html/thermostatcommand.php https://raw.githubusercontent.com/pindanet/Raspberry/master/domoticaController/var/www/html/thermostatcommand.php
   sudo apt-get -y install python-pip
   sudo pip install bme280
+  #echo 'dtoverlay=i2c-sensor,bme280,addr=0x77' | sudo tee -a /boot/config.txt
   
   sudo wget -O /var/www/html/tls2591.py https://raw.githubusercontent.com/pindanet/Raspberry/master/domoticaController/var/www/html/tls2591.py
   
