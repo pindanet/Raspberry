8c2c10ef910712d721863e6356e27d78301e3935
Dany Pinoy <dany.pinoy@pindanet.be>
2024-02-28 18:40:07 +0100

    Update slave_install.sh

--- slave_install.sh
+++ slave_install.sh
@@ -113,14 +113,16 @@ else
   
   sudo apt-get install i2c-tools bc -y
   i2cdetect -y 1
-  
-  sudo nano /boot/config.txt
-     dtparam=i2c_arm=on
-     dtoverlay=i2c-sensor,jc42
-#  sudo apt install python3-pip -y
-#  sudo pip3 install adafruit-circuitpython-mcp9808
-#  sudo wget -O /usr/sbin/mcp9808.py https://github.com/pindanet/Raspberry/raw/master/domoticaSlave/usr/sbin/mcp9808.py
-#  sudo chmod +x /usr/sbin/mcp9808.py
+
+# Werkt niet in Buster
+#  sudo nano /boot/config.txt
+#     dtparam=i2c_arm=on
+#     dtoverlay=i2c-sensor,jc42
+# Alternatief voor Buster
+  sudo apt install python3-pip -y
+  sudo pip3 install adafruit-circuitpython-mcp9808
+  sudo wget -O /usr/sbin/mcp9808.py https://github.com/pindanet/Raspberry/raw/master/domoticaSlave/usr/sbin/mcp9808.py
+  sudo chmod +x /usr/sbin/mcp9808.py
   
   sudo mkdir -p /var/www/html/data/
   sudo wget -O /var/www/html/data/thermostat https://raw.githubusercontent.com/pindanet/Raspberry/master/domoticaController/var/www/html/data/thermostat
