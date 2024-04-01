0b7432104643e9e468d04873ba86a59a19bb0dc9
Dany Pinoy <dany.pinoy@pindanet.be>
2024-04-01 15:46:24 +0200

    Update lights.js

--- domoticaController/var/www/html/lights.js
+++ domoticaController/var/www/html/lights.js
@@ -1,11 +1,40 @@
 var sunTimes;
-//console.log(times.sunrise.toString());
-//var sunrise = times.sunrise.getHours().toString().padStart(2, '0') + ':' + times.sunrise.getMinutes().toString().padStart(2, '0');
-//var sunset = times.sunset.getHours().toString().padStart(2, '0') + ':' + times.sunset.getMinutes().toString().padStart(2, '0');
 var nextAlarm;
 var breakfast;
 var morningLightsOut;
 var eveningLightsOn;
+var bedTime;
+function lightSwitch(name, cmd) {
+  var tasmotaSwitch = conf.switch[name];
+  var xhr = new XMLHttpRequest();
+  xhr.open('POST', "cli.php", true);
+  xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  xhr.onload = function(e) {
+    if (this.status == 200) {
+      if (this.responseText != "[]") { // no response
+        const output = JSON.parse(this.responseText);
+        if (output[0].includes(':"OFF"}')) {
+          tasmotaSwitch.status = "Off";
+        } else if (output[0].includes(':"ON"}')) {
+          tasmotaSwitch.status = "On";
+        }
+        powerLog(tasmotaSwitch, name);
+        if (cmd == "Toggle") {
+          if (Object.keys(tasmotaSwitch).includes("manual")) {
+            delete tasmotaSwitch.manual;
+          } else {
+            tasmotaSwitch.manual = tasmotaSwitch.status;
+          }
+        }
+      }
+    }
+  };
+  if (typeof tasmotaSwitch.Channel !== 'undefined') {
+    xhr.send("cmd=wget&params="+stringToHex("-qO- http://" + tasmotaSwitch.IP + "/cm?cmnd=Power"+ tasmotaSwitch.Channel + "%20" + cmd));
+  } else {
+    xhr.send("cmd=wget&params="+stringToHex("-qO- http://" + tasmotaSwitch.IP + "/cm?cmnd=Power%20" + cmd));
+  }
+}
 function getEventAlarm(nowDate, ref) {
   var nowDateOnly = new Date(nowDate.getTime());
   var refDate = new Date(ref.getTime());
@@ -30,29 +59,15 @@ function getEventAlarm(nowDate, ref) {
         var end = endDate.getTime();
         endDate.setTime(end + expired);
         endDate = timeDate(conf.event[i].end, endDate);
-//        var endTime = splitTime(conf.event[i].end);
-//        endDate.setHours(endTime[0]);
-//        endDate.setMinutes(endTime[1]);
-//        endDate.setSeconds(0);
-console.log(endDate.toString());
         end = endDate.getTime();
 
         beginDate.setTime(begin);
         beginDate = timeDate(conf.event[i].begin, beginDate);
-//        var beginTime = splitTime(conf.event[i].begin);
-//        beginDate.setHours(beginTime[0]);
-//        beginDate.setMinutes(beginTime[1]);
-console.log(beginDate.toString());
         begin = beginDate.getTime();
 
         var alarmDate = new Date();
         alarmDate.setTime(begin);
         alarmDate = timeDate(conf.event[i].alarm, alarmDate);
-//        var alarmTime = splitTime(conf.event[i].alarm);
-//        alarmDate.setHours(alarmTime[0]);
-//        alarmDate.setMinutes(alarmTime[1]);
-//        alarmDate.setSeconds(0);
-console.log(alarmDate.toString());
         alarm = alarmDate.getTime();
         if (alarm >= refDate.getTime() && alarm <= end) {
           return alarmDate.getTime();
@@ -65,11 +80,6 @@ console.log(alarmDate.toString());
 function setAlarmTime(nextAlarmSec, defaultDate) {
   if (nextAlarmSec == 0) { // No alarmtime found, use default alarmtime
     nextAlarm = timeDate (conf.alarmtime, new Date(defaultDate.getTime()));
-//    nextAlarm = new Date(defaultDate.getTime());
-//    var nextAlarmTime = splitTime(conf.alarmtime);
-//    nextAlarm.setHours(nextAlarmTime[0]);
-//    nextAlarm.setMinutes(nextAlarmTime[1]);
-//    nextAlarm.setSeconds(0);
   } else { // Set found alarmtime
     nextAlarm = new Date(nextAlarmSec);
   }
@@ -84,6 +94,11 @@ function nextalarm() {
     nextDate.setHours(0,0,0,0);
     setAlarmTime(getEventAlarm(nextDate, today), nextDate); // Get tomorrow's alarmtime
   }
+  var alarmtime = timeDate (conf.alarmtime, new Date());
+  if (today.getTime() < alarmtime.getTime()) { // still night, disable backlight Touchscreen
+    gotoSleep();
+  }
+  setTimeout(wakeup, nextAlarm.getTime() - today.getTime()); // activate backlight Touchscreen at nextAlarm
 
   var sunTimes = SunCalc.getTimes(nextAlarm, 51.2, 5);
   morningTimerLightsOut = new Date(nextAlarm.getTime() + (conf.lights.lightsOut.Offset * 60000)); // 79 min (1 hour 19 min) after wakeup
@@ -101,22 +116,37 @@ function nextalarm() {
   } else { // Still daylight
     eveningLightsOn = new Date(eveningShutterDown.getTime());
   }
-
-  lights();
 }
+//function sleepLightsOut() {
+//  for (const [key, value] of Object.entries(conf.switch)) {
+//    lightSwitch(key, "Off");
+//  }
+//  radioCommand(event, 'stop', 1);
+//}
 function lights() {
   var now = new Date().getTime();
+  for (const [key, value] of Object.entries(conf.switch)) { // reset switch commands
+   conf.switch[key].cmd = "Off";
+  }
   for (let i = 0; i < conf.lights.timer.length; i++) {
     var beginDate = timeDate(conf.lights.timer[i].begin, new Date());
     var begin = beginDate.getTime();
     var endDate = timeDate(conf.lights.timer[i].end, new Date());
     var end = endDate.getTime();
     if (begin <= now && end > now) {
-console.log(conf.lights.timer[i]);
-console.log(beginDate, endDate);
-console.log(conf.switch[conf.lights.timer[i].dev]);
+      conf.switch[conf.lights.timer[i].dev].cmd = "On";
     }
   }
-  setTimeout(lights, 60000);
+  for (const [key, value] of Object.entries(conf.switch)) {
+    if (! Object.keys(conf.switch[key]).includes("status")) {
+      lightSwitch(key, conf.switch[key].cmd);
+    } else {
+      if (! Object.keys(conf.switch[key]).includes("manual")) {
+        if (conf.switch[key].cmd != conf.switch[key].status) {
+          lightSwitch(key, conf.switch[key].cmd);
+        }
+      }
+    }
+  }
+   setTimeout(lights, 60000);
 }
-setTimeout(nextalarm, 6000);
