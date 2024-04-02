d11005b045b4ecaa54d104b9da85c1ba31ccdb1c
Dany Pinoy <dany.pinoy@pindanet.be>
2024-04-02 16:53:07 +0200

    Update domoticaController_install.sh

--- domoticaController_install.sh
+++ domoticaController_install.sh
@@ -199,6 +199,7 @@ EOF
   sudo wget -O /var/www/html/emoji/light_bulb.svg https://raw.githubusercontent.com/pindanet/Raspberry/master/domoticaController/var/www/html/emoji/light_bulb.svg
   sudo wget -O /var/www/html/emoji/ledlamp.svg https://raw.githubusercontent.com/pindanet/Raspberry/master/domoticaController/var/www/html/emoji/ledlamp.svg
   sudo wget -O /var/www/html/emoji/ledlampzij.svg https://raw.githubusercontent.com/pindanet/Raspberry/master/domoticaController/var/www/html/emoji/ledlampzij.svg
+  sudo wget -O /var/www/html/emoji/sleep.svg https://raw.githubusercontent.com/pindanet/Raspberry/master/domoticaController/var/www/html/emoji/sleep.svg
   sudo wget -O /var/www/html/background.php https://raw.githubusercontent.com/pindanet/Raspberry/master/domoticaController/var/www/html/background.php
   sudo wget -O /var/www/html/openssl.php https://raw.githubusercontent.com/pindanet/Raspberry/master/domoticaController/var/www/html/openssl.php
   sudo wget -O /var/www/html/photoframe.sh https://raw.githubusercontent.com/pindanet/Raspberry/master/domoticaController/var/www/html/photoframe.sh
