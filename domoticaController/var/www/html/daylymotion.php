fc6eff4c48ec98e7ae1bf8fc1f6b6ef03dec4b07
Dany Pinoy <dany.pinoy@pindanet.be>
2024-04-30 16:19:28 +0200

    Delete domoticaController/var/www/html/daylymotion.php

--- domoticaController/var/www/html/daylymotion.php
+++
@@ -1,9 +0,0 @@
-<?php
-function pathPrefix(&$value,$key) {
-  $value="motion/fotos/$value";
-}
-$files = array_diff(scandir("motion/fotos/"), array('..', '.'));
-sort($files);
-array_walk($files,"pathPrefix");
-echo json_encode($files);
-?>
