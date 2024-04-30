eea16b9748c5b6a6e5b507254c661d524b678957
Dany Pinoy <dany.pinoy@pindanet.be>
2024-04-30 16:19:39 +0200

    Delete domoticaController/var/www/html/exportthermostat.php

--- domoticaController/var/www/html/exportthermostat.php
+++
@@ -1,6 +0,0 @@
-<?php
-header("Content-Type: text/plain");
-header("Content-Transfer-Encoding: base64"); 
-header("Content-disposition: attachment; filename=\"thermostat\""); 
-readfile("/var/www/html/data/thermostat"); 
-?>
