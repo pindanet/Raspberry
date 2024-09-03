b66d53b8b14ba5eddfc03f727df081553f790572
Dany Pinoy <dany.pinoy@pindanet.be>
2024-09-03 18:50:46 +0200

    Update install.sh

--- domoticaController/install.sh
+++ domoticaController/install.sh
@@ -29,7 +29,7 @@ esac
 
 sudo apt update && sudo apt -y full-upgrade
 
-echo "Install Wayland"
+echo "Install Wayland" # https://gist.github.com/seffs/2395ca640d6d8d8228a19a9995418211
 sudo apt install wayfire seatd xdg-user-dirs libgl1-mesa-dri
 mkdir .config
 touch ~/.config/wayfire.init
