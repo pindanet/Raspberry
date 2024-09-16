b495162cb27d36f9c4eeb01baadd107d0804fc0d
Dany Pinoy <dany.pinoy@pindanet.be>
2024-09-16 15:18:41 +0200

    Update install.sh

--- domoticaSlave/install.sh
+++ domoticaSlave/install.sh
@@ -78,6 +78,7 @@ sudo cp -r Raspberry-master/domoticaSlave/var/www/html/* /var/www/html/
 rm -r Raspberry-master/
 
 sudo chmod +x /var/www/html/ds18b20.sh
+sudo chown www-data:www-data /var/www/html/data
 
 echo "Autostart fullscreen browser" # https://core-electronics.com.au/guides/raspberry-pi-kiosk-mode-setup/
 echo "============================"
