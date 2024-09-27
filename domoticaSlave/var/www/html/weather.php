a2e7b4285b73997815e9383fc0df721c57cf6013
Dany Pinoy <dany.pinoy@pindanet.be>
2024-09-27 18:34:00 +0200

    Update weather.php

--- domoticaSlave/var/www/html/weather.php
+++ domoticaSlave/var/www/html/weather.php
@@ -4,7 +4,10 @@
 
   function getWeather() {
 //    exec("curl -s 'wttr.in/Brugge?lang=nl&format=j1'", $output);
-    exec("curl -s 'https://api.met.no/weatherapi/locationforecast/2.0/complete?lat=51.20&lon=3.26'", $output);
+    $lat = htmlspecialchars($_POST["lat"]);
+    $lon = htmlspecialchars($_POST["lon"]);
+    $alt = htmlspecialchars($_POST["alt"]);
+    exec("curl -s 'https://api.met.no/weatherapi/locationforecast/2.0/complete?lat=" . $lat . "&lon=" . $lon . "&altitude=" . $alt . "'", $output);
     file_put_contents("/tmp/wttr", $output);
   }
   if (file_exists("/tmp/wttr")) { // first time
