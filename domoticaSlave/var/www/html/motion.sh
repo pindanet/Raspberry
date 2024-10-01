893ba728852e836a87f97a2f11b3f195dd90d106
Dany Pinoy <dany.pinoy@pindanet.be>
2024-10-01 18:23:37 +0200

    Create motion.sh

---
+++ domoticaSlave/var/www/html/motion.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+if [ ! -d motion ]; then
+  mkdir motion
+fi
+find ./motion -mtime +1 -type f -delete
+filename=$(date +"%Y-%m-%d_%H.%M.%S.jpg")
+rpicam-still -o motion/$filename --width 480 --height 800
