0ffeb2b4355874e4ab7521c6dfe404f793e41446
Dany Pinoy <dany.pinoy@pindanet.be>
2024-11-04 14:28:03 +0100

    Update install.sh

--- domoticaSlave/install.sh
+++ domoticaSlave/install.sh
@@ -98,9 +98,9 @@ echo "Autostart fullscreen browser" # https://core-electronics.com.au/guides/ras
 echo "============================"
 sudo apt install chromium -y
 echo "/usr/bin/pinctrl set $powergpio op dh" >> .config/labwc/autostart
-echo "/bin/chromium-browser --kiosk --ozone-platform=wayland --start-maximized --noerrdialogs --disable-infobars --enable-features=OverlayScrollbar  http://localhost/ &" >> .config/labwc/autostart
+echo "/bin/chromium --kiosk --ozone-platform=wayland --start-maximized --noerrdialogs --disable-infobars --enable-features=OverlayScrollbar  http://localhost/ &" >> .config/labwc/autostart
 # debug mode
-# echo "/usr/bin/pinctrl set 17 op dh; /bin/chromium-browser --remote-debugging-port=9222 --kiosk --ozone-platform=wayland --start-maximized --noerrdialogs --disable-infobars --enable-features=OverlayScrollbar  http://localhost/ &" > .config/labwc/autostart
+# echo "/usr/bin/pinctrl set 17 op dh; /bin/chromium --remote-debugging-port=9222 --kiosk --ozone-platform=wayland --start-maximized --noerrdialogs --disable-infobars --enable-features=OverlayScrollbar  http://localhost/ &" > .config/labwc/autostart
 
 echo "Configure SSH remote login"
 echo "=========================="
