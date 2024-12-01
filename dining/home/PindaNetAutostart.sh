6357d994a83027bc8a6c826fc3afb3320e0c0b34
Dany Pinoy <dany.pinoy@pindanet.be>
2024-12-01 18:54:39 +0100

    Update PindaNetAutostart.sh

--- dining/home/PindaNetAutostart.sh
+++ dining/home/PindaNetAutostart.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
+killall vlc
+killall wayout
 # Optional Force resolution
 # sudo apt install wlr-randr -y
 # Only for my EIZO monitor
 #wlr-randr --output HDMI-A-1 --mode 1920x1200@59.950001Hz
 # Activate DS18B20 temperature sensor power (Reset)
-/usr/bin/pinctrl set  op dh
+/usr/bin/pinctrl set 17 op dh
 # PullUp 1-wire Data
 /usr/bin/pinctrl set 4 ip pu
-# Autostart Chromium in Kiosk & Debug mode
-#/bin/chromium --autoplay-policy=no-user-gesture-required --remote-debugging-port=9222 --kiosk --ozone-platform=wayland --start-maximized --noerrdialogs --disable-infobars --enable-features=OverlayScrollbar  http://localhost/ &
-# Give Chromium time to start
+# Autostart VLC in Kiosk mode
 sleep 30
-ffplay -hide_banner -loglevel error -fs -loop 0 /var/www/html/haardvuur.mp4
-# Check if Chromium is running
-#until ps -ax | grep kiosk | grep -v grep
-#do
-#  # After a hostname change, chromium refuses to start, correct this
-#  rm -rf /home/dany/.config/chromium/Singleton*
-#  # Restart chromium
-#  /bin/chromium --autoplay-policy=no-user-gesture-required --remote-debugging-port=9222 --kiosk --ozone-platform=wayland --start-maximized --noerrdialogs --disable-infobars --enable-features=OverlayScrollbar  http://localhost/ &
-#  sleep 30
-#done
+videos+=("aquarium.mp4")
+videos+=("haardvuur.mp4")
+video=${videos[$(( $RANDOM % ${#videos[@]} ))]}
+if [ $video == "aquarium.mp4" ]; then
+  color="black"
+else
+  color="white"
+fi
+vlc --no-video-title-show --fullscreen --loop -Idummy /var/www/html/${video} &
+# --alsa-audio-device default
+while true; do echo "<span foreground=\"${color}\">$(date +%H:%M:%S)</span>"; sleep 1; done | wayout --feed-line --width 800 --height 40 --layer overlay --position bottom --font "Monospace 22"
