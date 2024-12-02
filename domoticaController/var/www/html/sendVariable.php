68b642131c3683ad24a41abcbef5e69660d8f227
Dany Pinoy <dany.pinoy@pindanet.be>
2024-12-02 15:07:10 +0100

    Create sendVariable.php

---
+++ domoticaController/var/www/html/sendVariable.php
@@ -0,0 +1,17 @@
+<?php
+$json = file_get_contents('php://input');
+$data = json_decode($json, true);
+// Check if decoding was successful
+if ($data !== null) {
+  copy("data/conf.json","data/variable.json.bak");
+  // Perform further processing or respond to the request
+  if (file_put_contents("data/variable.json", $json))
+    echo "Variable is saved.";
+  else
+    echo "Oops! Error creating json file...";
+} else {
+   // JSON decoding failed
+   http_response_code(400); // Bad Request
+   echo "Invalid JSON data";
+}
+?>
