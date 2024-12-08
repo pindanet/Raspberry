5a236d54acba879511b487d6fcc528eca2efe4d4
Dany Pinoy <dany.pinoy@pindanet.be>
2024-12-08 14:06:41 +0100

    Create ds18b20.sh

---
+++ dining/var/www/html/ds18b20.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+# DS18B20
+# GPIO 17 (11) (switchable 3,3 V) naar Vdd (Rood)
+# GPIO4 (7) naar Data (Geel) naar 4k7 naar 3,3 V (Orange)(GPIO27)
+# GND (9) naar GND (Zwart)
+
+powergpio=17
+
+if [ ! -d data/temp.log ]; then
+  mkdir -p data/temp.log
+fi
+
+#cat /sys/devices/w1_bus_master1/28-*/temperature
+#temp=$(cat /sys/bus/w1/devices/28-*/temperature)
+output=$(cat /sys/bus/w1/devices/28-*/w1_slave)
+if [ $? -ne 0 ]; then # error
+  # Reset DS18B20
+  # Power off
+  pinctrl set $powergpio op dl
+  sleep 3
+  # Power on
+  pinctrl set $powergpio op dh
+  sleep 5
+  echo "error"
+  exit
+fi
+
+crc=$(echo "${output}" | head -1)
+if [[ $crc == *"YES" ]]; then
+  temp="${output#*t=}"
+else
+  echo "crc"
+  exit
+fi
+
+echo $temp
+echo $temp > /var/www/html/data/temp
+
+# minimum maximum temp
+timestamp=$(date +"%m-%d_")
+if [ ! -f /var/www/html/data/temp.log/${timestamp}tempmax ]; then
+  echo 0 > /var/www/html/data/temp.log/${timestamp}tempmax
+fi
+tempmax=$(cat /var/www/html/data/temp.log/${timestamp}tempmax)
+if [ ! -f /var/www/html/data/temp.log/${timestamp}tempmin ]; then
+  echo 100000 > /var/www/html/data/temp.log/${timestamp}tempmin
+fi
+tempmin=$(cat /var/www/html/data/temp.log/${timestamp}tempmin)
+if [ ${temp%.*} -eq ${tempmax%.*} ] && [ ${temp#*.} \> ${tempmax#*.} ] || [ ${temp%.*} -gt ${tempmax%.*} ]; then
+  tempmax=$temp
+  echo $tempmax > /var/www/html/data/temp.log/${timestamp}tempmax
+fi
+if [ ${temp%.*} -eq ${tempmin%.*} ] && [ ${temp#*.} \< ${tempmin#*.} ] || [ ${temp%.*} -lt ${tempmin%.*} ]; then
+  tempmin=$temp
+  echo $tempmin > /var/www/html/data/temp.log/${timestamp}tempmin
+fi
