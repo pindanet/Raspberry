5dc18448e57066ba1d31d436cf5e2b42256c08b8
Dany Pinoy <dany.pinoy@pindanet.be>
2024-12-10 19:38:10 +0100

    Update autostart.sh

--- dining/var/www/html/autostart.sh
+++ dining/var/www/html/autostart.sh
@@ -10,8 +10,6 @@ killall wayout
 /usr/bin/pinctrl set 27 op dh
 # PullUp 1-wire Data
 /usr/bin/pinctrl set 4 ip pu
-# Autostart VLC in Kiosk mode
-sleep 30
 readarray -d '' videos < <(find /var/www/html/ -name "*.mp4" -print0)
 video=${videos[$(( $RANDOM % ${#videos[@]} ))]}
 if [[ "$video" == *"aquarium"* ]]; then
@@ -19,9 +17,24 @@ if [[ "$video" == *"aquarium"* ]]; then
 else
   color="white"
 fi
-vlc --no-video-title-show --fullscreen --loop -Idummy ${video} &
+
+#video="/var/www/html/haardvuur.mp4"
+#color="white"
+
 while true; do
-  text=$(date +%H:%M:%S)$(printf " %.1f" $((10**1 * $(($(cat /var/www/html/data/temp) + 700))/1000))e-1)
-  echo "<b><span foreground=\"${color}\">${text}</span></b>"
+  # Wait for active HDMI
+  if [ $(cat /sys/class/drm/card?-HDMI-A-1/enabled) == "enabled" ]; then
+    echo "Start VLC"
+    # Start VLC met Wayland in Kiosk mode
+    unset DISPLAY
+    QT_QPA_PLATFORM=wayland vlc --no-video-title-show --fullscreen --loop -Idummy ${video} &
+    while [ $(cat /sys/class/drm/card?-HDMI-A-1/enabled) == "enabled" ]; do
+      text=$(date +%H:%M:%S)$(printf " %.1f" $((10**1 * $(($(cat /var/www/html/data/temp) + 700))/1000))e-1)
+      echo "<b><span foreground=\"${color}\">${text}</span></b>"
+      sleep 1
+    done | wayout --feed-line --width 1920 --height 80 --layer overlay --position bottom --font "Monospace 44"
+    echo "Stop VLC"
+    killall vlc
+  fi
   sleep 1
-done | wayout --feed-line --width 1920 --height 80 --layer overlay --position bottom --font "Monospace 44"
+done
