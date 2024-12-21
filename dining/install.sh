f66247e9c0d7144650becb159a7d79510c4f9ff3
Dany Pinoy <dany.pinoy@pindanet.be>
2024-12-21 15:20:43 +0100

    Update install.sh

--- dining/install.sh
+++ dining/install.sh
@@ -54,7 +54,7 @@ fi
 
 echo "Shutdown/Boot button"
 echo "===================="
-echo 'dtoverlay=gpio-shutdown,gpio_pin=26' | sudo tee -a /boot/firmware/config.txt
+echo 'dtoverlay=gpio-shutdown' | sudo tee -a /boot/firmware/config.txt
 
 echo "Install webserver"
 echo "================="
