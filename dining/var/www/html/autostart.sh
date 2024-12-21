7c0a0ba5ec90b7a995c65515c10ec896e28bc058
Dany Pinoy <dany.pinoy@pindanet.be>
2024-12-21 17:41:39 +0100

    Update autostart.sh

--- dining/var/www/html/autostart.sh
+++ dining/var/www/html/autostart.sh
@@ -21,6 +21,9 @@ fi
 #video="/var/www/html/haardvuur.mp4"
 #color="white"
 
+days=("maandag" "dinsdag" "woensdag" "donderdag" "vrijdag" "zaterdag" "zondag")
+months=("januari" "februari" "maard" "april", "mei", "juni", "juli", "augustus", "september", "october", "november", "december")
+
 while true; do
   # Wait for active HDMI
   if [ $(cat /sys/class/drm/card?-HDMI-A-1/enabled) == "enabled" ]; then
@@ -28,8 +31,16 @@ while true; do
     # Start VLC met Wayland in Kiosk mode
     unset DISPLAY
     QT_QPA_PLATFORM=wayland vlc --no-video-title-show --fullscreen --loop -Idummy ${video} &
+    locDate=${days[$(($(date '+%u') - 1))]}$(date "+, %-d ${months[$(($(date '+%m') - 1))]} %Y")
+    counter=$(((33 - ${#locDate}) / 2))
+    while [ $counter -gt 0 ]
+    do
+      locDate=" $locDate"
+      ((counter=counter-1))
+    done
     while [ $(cat /sys/class/drm/card?-HDMI-A-1/enabled) == "enabled" ]; do
-      text=$(date +%H:%M:%S)$(printf " %.1f" $((10**1 * $(($(cat /var/www/html/data/temp) + 700))/1000))e-1)
+#      text="$(LANG=nl_BE.UTF-8 date '+%A, %e %B %Y %H:%M:%S')     $(printf ' %.1f   C' $((10**1 * $(($(cat /var/www/html/data/temp) + 700))/1000))e-1)"
+      text="${locDate}   $(date '+ %H:%M:%S')   $(printf ' %.1f \u00B0C' $((10**1 * $(($(cat /var/www/html/data/temp) + 700))/1000))e-1)"
       echo "<b><span foreground=\"${color}\">${text}</span></b>"
       sleep 1
     done | wayout --feed-line --width 1920 --height 80 --layer overlay --position bottom --font "Monospace 44"
