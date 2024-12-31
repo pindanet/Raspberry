60b002b511bdb9b342764408cf24b0689d677073
Dany Pinoy <dany.pinoy@pindanet.be>
2024-12-31 19:02:05 +0100

    Update motion.sh

--- domoticaSlave/var/www/html/motion.sh
+++ domoticaSlave/var/www/html/motion.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
-if [ ! -d motion ]; then
-  mkdir motion
+brightness=$(cat /sys/class/backlight/10-0045/brightness)
+if [ $? -ne 0 ]; then # error
+  echo "error"
+  exit
 fi
-find ./motion -mmin +$((60*24)) -type f -delete
-filename=$(date +"%Y-%m-%d_%H.%M.%S.jpg")
-rpicam-still -o motion/$filename --rotation 180 --immediate
-convert motion/$filename -resize 1920 motion/$filename
+if [ $brightness  -gt 0 ]; then
+  if [ ! -d /var/www/html/motion ]; then
+    mkdir /var/www/html/motion
+  fi
+  find /var/www/html/motion -mmin +$((60*24)) -type f -delete
+  filename=$(date +"%Y-%m-%d_%H.%M.%S.jpg")
+  /usr/bin/rpicam-still -o /var/www/html/motion/$filename --rotation 180 --immediate
+  /usr/bin/convert /var/www/html/motion/$filename -resize 1920 /var/www/html/motion/$filename
+fi
