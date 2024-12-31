59dd18180cd163820a9e7c492c77c7bf28e33aa9
Dany Pinoy <dany.pinoy@pindanet.be>
2024-12-31 17:54:31 +0100

    Update motion.sh

--- domoticaSlave/var/www/html/motion.sh
+++ domoticaSlave/var/www/html/motion.sh
@@ -2,7 +2,7 @@
 if [ ! -d motion ]; then
   mkdir motion
 fi
-find ./motion -mtime +1 -type f -delete
+find ./motion -mmin +$((60*24)) -type f -delete
 filename=$(date +"%Y-%m-%d_%H.%M.%S.jpg")
 rpicam-still -o motion/$filename --rotation 180 --immediate
 convert motion/$filename -resize 1920 motion/$filename
