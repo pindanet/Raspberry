643b81fbd1f652c0ed56afa09472119aa4e21184
Dany Pinoy <dany.pinoy@pindanet.be>
2025-01-01 16:43:42 +0100

    Update install.sh

--- domoticaController/install.sh
+++ domoticaController/install.sh
@@ -53,7 +53,12 @@ if [ $? == 1 ]; then
   echo 'w1-therm' | sudo tee -a /etc/modules
 fi
 
+echo "Shutdown/Boot button"
+echo "===================="
+echo 'dtoverlay=gpio-shutdown' | sudo tee -a /boot/firmware/config.txt
+
 echo "Install webserver"
+echo "================="
 sudo apt install apache2 libapache2-mod-fcgid php-bcmath php-bz2 php-common php-curl php-xml php-gd php-php-gettext php-gmp php-ldap php-mbstring php-mysql php-odbc php-pgsql php-snmp php-soap php-sqlite3 php-tokenizer libapache2-mod-php -y
 
 if test -f master.zip; then rm master.zip; fi
