177267633d9bbcffe02c5e71bbc0cfc689e8c285
Dany Pinoy <dany.pinoy@pindanet.be>
2025-01-19 15:30:24 +0100

    Update Tasmota Network Scanner.sh

--- util/Tasmota Network Scanner.sh
+++ util/Tasmota Network Scanner.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 # Scan the given network for Tasmota devices
 # Prints the IP address and Hostname of the Tasmota device.
+if [ -z "$1" ]; then
+  set -- $(ip -o -f inet addr show | awk '/scope global/ {print $4}' | sed 's/\(.*\)\..*/\1/')
+fi
+if [[ ! $1 =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+  echo "Networkrange not accepted."
+  echo "Use bash Tasmota\ Network\ Scanner.sh 123.123.123"
+  exit
+fi
 for ip in {1..254}; do
-  # Stuur een door de gezochte apparaten herkend bericht.
+  # Send Tasmota http command to device
   status=$(wget -qO- http://$1.$ip/cm?cmnd=status%205)
-  # Filter uit het antwoord de hostname (enkel bruikbaar voor Tasmota apparaten).
+  # Extract the Hostname from the reply.
   hostname=$(echo $status | grep -o '"Hostname":"[^"]*' | grep -o '[^"]*$')
-  # Indien het apparaat een zinnig antwoord gaf, drukken we het IP adres en de hostname af.
+  # We found a Tasmota device if a Hostname could be extracted.
   if [ "$?" -eq 0 ]; then
     echo $1.$ip $hostname
   fi
