29352317e4b263dbb70f5039b0172e94ad12af6a
Dany Pinoy <dany.pinoy@pindanet.be>
2025-01-20 18:31:16 +0100

    Create tasmotaNetScan.sh

---
+++ domoticaController/var/www/html/tasmotaNetScan.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Scan the given network for Tasmota devices
+# Prints the IP address and Hostname of the Tasmota device.
+if [ -z "$1" ]; then
+  set -- $(ip -o -f inet addr show | awk '/scope global/ {print $4}' | sed 's/\(.*\)\..*/\1/')
+fi
+if [[ ! $1 =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+  echo "Networkrange not accepted."
+  echo "Use bash Tasmota\ Network\ Scanner.sh 123.123.123"
+  exit
+fi
+for ip in {1..254}; do
+  # Send Tasmota http command to device
+  status=$(wget -qO- http://$1.$ip/cm?cmnd=status%205)
+  # Extract the Hostname from the reply.
+  hostname=$(echo $status | grep -o '"Hostname":"[^"]*' | grep -o '[^"]*$')
+  # We found a Tasmota device if a Hostname could be extracted.
+  if [ "$?" -eq 0 ]; then
+    echo $1.$ip $hostname
+  fi
+done
