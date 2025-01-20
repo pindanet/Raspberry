c1b137357485f3c0ce3202f2909a9b1642f398a1
Dany Pinoy <dany.pinoy@pindanet.be>
2025-01-20 18:47:48 +0100

    Update tasmotaNetScan.sh

--- domoticaController/var/www/html/tasmotaNetScan.sh
+++ domoticaController/var/www/html/tasmotaNetScan.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Scan the given network for Tasmota devices
 # Prints the IP address and Hostname of the Tasmota device.
+untilIP=50
 if [ -z "$1" ]; then
   set -- $(ip -o -f inet addr show | awk '/scope global/ {print $4}' | sed 's/\(.*\)\..*/\1/')
 fi
@@ -9,7 +10,8 @@ if [[ ! $1 =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
   echo "Use bash Tasmota\ Network\ Scanner.sh 123.123.123"
   exit
 fi
-for ip in {1..254}; do
+#for ip in {1..50}; do
+for (( ip=2; ip<=$untilIP; ip++)); do
   # Send Tasmota http command to device
   status=$(wget -qO- http://$1.$ip/cm?cmnd=status%205)
   # Extract the Hostname from the reply.
