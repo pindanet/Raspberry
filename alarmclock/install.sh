1c90db0f14716774b082e24008fc9748bb04736c
Dany Pinoy <dany.pinoy@pindanet.be>
2025-01-24 16:55:56 +0100

    Update install.sh

--- alarmclock/install.sh
+++ alarmclock/install.sh
@@ -55,7 +55,6 @@ echo 'dtoverlay=gpio-shutdown' | sudo tee -a /boot/firmware/config.txt
 echo "Install webserver"
 echo "================="
 sudo apt install apache2 libapache2-mod-fcgid php-bcmath php-bz2 php-common php-curl php-xml php-gd php-php-gettext php-gmp php-ldap php-mbstring php-mysql php-odbc php-pgsql php-snmp php-soap php-sqlite3 php-tokenizer libapache2-mod-php -y
-sudo apt install imagemagick -y
 
 if test -f master.zip; then rm master.zip; fi
 echo "Download and extract Github Repository"
