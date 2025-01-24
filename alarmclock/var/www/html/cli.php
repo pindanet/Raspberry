25a919b130b9bef54ef9f4b260b0f2db810b0823
Dany Pinoy <dany.pinoy@pindanet.be>
2025-01-24 18:45:05 +0100

    Create cli.php

---
+++ alarmclock/var/www/html/cli.php
@@ -0,0 +1,18 @@
+<?php
+
+// Use terminal arg as POST en GET arg, example: php /var/www/html/cli.php cmd=wget params=$(echo -n '-qO- http://192.168.129.41/cm?cmnd=Power3%20Off' | xxd -p -c 256)
+if (!isset($_SERVER["HTTP_HOST"])) {
+  parse_str(implode('&', array_slice($argv, 1)), $_GET);
+  parse_str(implode('&', array_slice($argv, 1)), $_POST);
+}
+
+$params = hex2bin(htmlspecialchars($_POST["params"]));
+$cmd = htmlspecialchars($_POST["cmd"]);
+
+//file_put_contents("data/debug.txt", $cmd . " " . $params);
+
+// Filter op toegelaten opdrachten
+
+//echo $cmd." ".$params;
+exec($cmd." ".$params, $output, $return);
+echo json_encode($output);
