9516bfd633b241e1d79cd1070efd84f916f539c4
Dany Pinoy <dany.pinoy@pindanet.be>
2025-01-25 17:51:05 +0100

    Update index.js

--- alarmclock/var/www/html/index.js
+++ alarmclock/var/www/html/index.js
@@ -1,4 +1,3 @@
-//var alarmTime = "07:30";
 var startTimer;
 var dayNames = new Array("Zondag","Maandag","Dinsdag","Woensdag","Donderdag","Vrijdag","Zaterdag");
 var monthNames = new Array("januari","februari","maart","april","mei","juni","juli","augustus","september","oktober","november","december");
@@ -53,6 +52,105 @@ console.log(this.responseText);
   startTime();
 }
 
+function timeDate (time, dateObject) {
+  var hourMin = [];
+  if (time.indexOf(":") > -1) {
+    hourMin = time.split(':');
+  } else {
+    if (conf.hasOwnProperty(time)) {
+      hourMin = conf[time].split(':');
+    } else {
+      var varDate = new Date();
+      varDate.setTime(window[time].getTime());
+      hourMin[0] = varDate.getHours();
+      hourMin[1] = varDate.getMinutes();
+    }
+  }
+  dateObject.setHours(hourMin[0], hourMin[1], 0, 0);
+  return dateObject;
+}
+
+function getEventAlarm(nowDate, ref) {
+  var nowDateOnly = new Date(nowDate.getTime());
+  var refDate = new Date(ref.getTime());
+  for (let i = 0; i < conf.event.length; i++) {
+    if (conf.event[i].hasOwnProperty('alarm')) {
+      nowDateOnly.setHours(0);
+      nowDateOnly.setMinutes(0);
+      nowDateOnly.setSeconds(0);
+      nowDateOnly.setMilliseconds(0);
+      var dateOnly = nowDateOnly.getTime();
+      var beginDate = new Date(conf.event[i].begindate);
+      beginDate.setHours(0);
+      var begin = beginDate.getTime();
+      if (conf.event[i].repeat > 0) { // repeating event
+        while (begin < dateOnly) {
+//          begin += 86400000 * conf.event[i].repeat;
+          newBeginDate = new Date(begin);
+          newBeginDate.setDate(newBeginDate.getDate() + conf.event[i].repeat);
+          begin = newBeginDate.getTime();
+        }
+      }
+      if (begin == dateOnly) {
+        var expired = begin - beginDate.getTime();
+        var endDate = new Date(conf.event[i].enddate);
+        var end = endDate.getTime();
+        endDate.setTime(end + expired);
+        endDate = timeDate(conf.event[i].end, endDate);
+        end = endDate.getTime();
+
+        beginDate.setTime(begin);
+        beginDate = timeDate(conf.event[i].begin, beginDate);
+        begin = beginDate.getTime();
+
+        var alarmDate = new Date();
+        alarmDate.setTime(begin);
+        alarmDate = timeDate(conf.event[i].alarm, alarmDate);
+        alarm = alarmDate.getTime();
+        if (alarm >= refDate.getTime() && alarm <= end) {
+          return alarmDate.getTime();
+        }
+      }
+    }
+  }
+  return 0;
+}
+
+function setAlarmTime(nextAlarmSec, defaultDate) {
+  if (nextAlarmSec == 0) { // No alarmtime found, use default alarmtime
+    nextAlarm = timeDate (conf.alarmtime, new Date(defaultDate.getTime()));
+  } else { // Set found alarmtime
+    nextAlarm = new Date(nextAlarmSec);
+  }
+}
+
+function setBrightness(brightness) {
+  var xhr = new XMLHttpRequest();
+  xhr.open('POST', "brightness.php", true);
+  xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  xhr.send("brightness=" + brightness);
+}
+
+var nextAlarm;
+function nextalarm() {
+  var today = new Date();
+  setAlarmTime(getEventAlarm(today, today), today); // Get today's alarmtime
+  if (today > nextAlarm) { // Alarmtime has expired
+    var nextDate = new Date();
+    nextDate.setDate(nextAlarm.getDate() + 1);
+    nextDate.setHours(0,0,0,0);
+    setAlarmTime(getEventAlarm(nextDate, today), nextDate); // Get tomorrow's alarmtime
+  }
+
+  sunTimes = SunCalc.getTimes(new Date(), conf.location.Latitude, conf.location.Longitude, conf.location.Altitude);
+  if (today.getTime() > sunTimes.sunset.getTime()) { // night, dim backlight Touchscreen
+    setBrightness(20);
+  } else { //daylight
+    setBrightness(64);
+    setTimeout(setBrightness, sunTimes.sunset.getTime() - today.getTime(), 20); // dim backlight Touchscreen at sunset
+  }
+}
+
 function startTime() {
   clearTimeout(startTimer);
   var today = new Date();
@@ -62,30 +160,39 @@ function startTime() {
   var s = today.getSeconds();
   s = checkTime(s);
 
-  document.getElementById("clockdate").innerHTML = today.getDate() + '&nbsp;' + monthNames[today.getMonth()] + '&nbsp;' + today.getFullYear();
-  document.getElementById('clockday').innerHTML = dayNames[today.getDay()] + ' ' + nextAlarm;
   document.getElementById('clock').innerHTML = h + ":" + m + ":" + s;
 
   updateScreen++;
   if (updateScreen > 10) {
+    document.getElementById("clockdate").innerHTML = today.getDate() + '&nbsp;' + monthNames[today.getMonth()] + '&nbsp;' + today.getFullYear();
+    document.getElementById('clockday').innerHTML = dayNames[today.getDay()] + ' ' + nextAlarm.getHours() + ":" + checkTime(nextAlarm.getMinutes());
     updateScreen = 0;
     radioStatus();
-//    var xhr = new XMLHttpRequest();
-//    xhr.open('POST', "data/radio.log", true);
-//    xhr.onload = function(e) {
-//      if (this.status == 200) {
-//        var lines = this.responseText.split("\n");
-//        var status = lines[lines.length - 2].split("'")[1];
-//        var fontsize = 10;
-//        if (status.length > 16) {
-//          fontsize = 5;
-//        }
-//console.log(status);
-//        document.getElementById('radio').innerHTML = "<span style='font-size:" + fontsize + "vw'>" + status + "</span>";
-//      }
-//    };
-//    xhr.send();
   }
   startTimer = setTimeout(startTime, 1000); // elke seconde
 }
-window.onload = getNextAlarm;
+
+function getConf() { // Get configuration
+  const xhttp = new XMLHttpRequest();
+  xhttp.onload = function(e) {
+    if (this.status == 200) {
+      if (typeof conf === 'undefined') {
+        conf = JSON.parse(this.responseText);
+        conf.lastModified = this.getResponseHeader('Last-Modified');
+//        calcConf();
+        nextalarm();
+        startTime();
+      } else if (conf.lastModified !== this.getResponseHeader('Last-Modified')) { // new configuration
+        conf = JSON.parse(this.responseText);
+        conf.lastModified = this.getResponseHeader('Last-Modified');
+//        calcConf();
+        nextalarm();
+      }
+//      variableToConf(variable, conf);
+      setTimeout(getConf, 60000); // Every minute
+    }
+  }
+  xhttp.open("POST", "data/conf.json");
+  xhttp.send();
+}
+window.onload = getConf;
