32a562b8f5b8f3284597536952f533bcf37f5280
Dany Pinoy <dany.pinoy@pindanet.be>
2025-01-28 19:28:32 +0100

    Update PindaNetRadio.sh

--- alarmclock/var/www/html/PindaNetRadio.sh
+++ alarmclock/var/www/html/PindaNetRadio.sh
@@ -12,7 +12,19 @@ volume=500
 
 mpg123 -f -$volume /var/www/html/data/bintro.mp3
 
-#nohup bash /var/www/html/playRadio.sh "$radio" $volume 2> /var/www/html/data/radio.log &
-
 interval="8192"
-curl -H "Icy-MetaData:1" --silent -L "$radio" 2>&1 | mpg123 --icy-interval $interval -f -$volume - 2> /var/www/html/data/radio.log
+curl -H "Icy-MetaData:1" --silent -L "$radio" 2>&1 | mpg123 --icy-interval $interval -f -$volume - 2> /var/www/html/data/radio.log &
+
+sleep 180 # 3 minuten wakker worden
+sleep 180 # 3 minuten nekoefeningen
+sleep 300 # 5 minuten rechtop zitten
+
+sudo dpkg --configure -a
+apt-get clean
+apt autoremove -y
+apt-get update
+apt-get upgrade -y
+
+rm /var/www/html/data/radio.log
+
+shutdown -r now
