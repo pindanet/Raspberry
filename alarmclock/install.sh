3b9d3b62cd644685bf4cc02ce7c28e3675715720
Dany Pinoy <dany.pinoy@pindanet.be>
2025-01-28 15:49:42 +0100

    Update install.sh

--- alarmclock/install.sh
+++ alarmclock/install.sh
@@ -62,6 +62,7 @@ echo "Install webserver"
 echo "================="
 sudo apt install apache2 libapache2-mod-fcgid php-bcmath php-bz2 php-common php-curl php-xml php-gd php-php-gettext php-gmp php-ldap php-mbstring php-mysql php-odbc php-pgsql php-snmp php-soap php-sqlite3 php-tokenizer libapache2-mod-php -y
 sudo apt install mpg123 -y
+# sudo apt install screen -y
 
 if test -f master.zip; then rm master.zip; fi
 echo "Download and extract Github Repository"
