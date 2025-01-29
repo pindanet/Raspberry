2503ed3f582e7b75247a3349ef75d1de7501c01f
Dany Pinoy <dany.pinoy@pindanet.be>
2025-01-29 19:34:20 +0100

    Update PindaNetRadio.sh

--- alarmclock/var/www/html/PindaNetRadio.sh
+++ alarmclock/var/www/html/PindaNetRadio.sh
@@ -1,30 +1,51 @@
 #!/bin/bash
 # AlarmClock Alarm
 
-radio2="http://icecast.vrtcdn.be/ra2wvl-high.mp3"
-klara="http://icecast.vrtcdn.be/klara-high.mp3"
-stubru="http://icecast.vrtcdn.be/stubru-high.mp3"
+str=$(cat /var/www/html/data/radio.cmd)
+cmd=();
+delimiter=", "
+s=$str$delimiter
+while [[ $s ]]; do
+  cmd+=( "${s%%"$delimiter"*}" );
+  s=${s#*"$delimiter"};
+done;
+declare -p cmd
 
-radio=$klara
-sleepradio=$radio2
+case ${cmd[0]} in
+  "play")
+     volume=${cmd[1]}
+     interval=${cmd[2]}
+     radio=${cmd[3]}
 
-volume=500
+     mpg123 -f -${cmd[1]} /var/www/html/data/bintro.mp3
 
-mpg123 -f -$volume /var/www/html/data/bintro.mp3
+     curl -H "Icy-MetaData:1" --silent -L "$radio" 2>&1 | mpg123 --icy-interval $interval -f -$volume - 2> /var/www/html/data/radio.log &
 
-interval="8192"
-curl -H "Icy-MetaData:1" --silent -L "$radio" 2>&1 | mpg123 --icy-interval $interval -f -$volume - 2> /var/www/html/data/radio.log &
+     sleep 30
 
-sleep 180 # 3 minuten wakker worden
-sleep 180 # 3 minuten nekoefeningen
-sleep 300 # 5 minuten rechtop zitten
+   ;;
 
-sudo dpkg --configure -a
-apt-get clean
-apt autoremove -y
-apt-get update
-apt-get upgrade -y
+  "alarm")
+    volume=${cmd[1]}
+    interval=${cmd[2]}
+    radio=${cmd[3]}
 
-rm /var/www/html/data/radio.log
+    mpg123 -f -${cmd[1]} /var/www/html/data/bintro.mp3
 
-shutdown -r now
+    curl -H "Icy-MetaData:1" --silent -L "$radio" 2>&1 | mpg123 --icy-interval $interval -f -$volume - 2> /var/www/html/data/radio.log &
+
+    sleep 180 # 3 minuten wakker worden
+    sleep 180 # 3 minuten nekoefeningen
+    sleep 300 # 5 minuten rechtop zitten
+
+    dpkg --configure -a
+    apt-get clean
+    apt autoremove -y
+    apt-get update
+    apt-get upgrade -y
+
+    rm /var/www/html/data/radio.log
+
+    shutdown -r now
+  ;;
+esac
