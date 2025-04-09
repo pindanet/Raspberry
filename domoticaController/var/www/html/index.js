3ef85423be05a85af79186f50f08d9181a3d2968
Dany Pinoy <dany.pinoy@pindanet.be>
2025-04-09 17:59:23 +0200

    Update index.js

--- domoticaController/var/www/html/index.js
+++ domoticaController/var/www/html/index.js
@@ -200,19 +200,27 @@ function radio(event) {
   radioVolume(event, 'getvol');
   radioStatus();
 }
-function radioStop(event) {
+//function radioStop(event) {
+//  var xhr = new XMLHttpRequest();
+//  xhr.open('POST', "cli.php", true);
+//  xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+//  xhr.send("cmd=rm&params="+stringToHex("/var/www/html/data/radio.log; killall mpg123 curl"));
+//  toTop();
+////  window.scrollTo(0, 0);
+////  document.getElementById('miniclock').style.display = 'none';
+////  document.getElementById('minitemp').style.display = 'none';
+//  radioVolume(event, conf.tvvolume);
+//  if (typeof event !== 'undefined') {
+//    event.stopPropagation();
+//  }
+//}
+function radioStop() {
   var xhr = new XMLHttpRequest();
   xhr.open('POST', "cli.php", true);
   xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-  xhr.send("cmd=rm&params="+stringToHex("/var/www/html/data/radio.log; killall mpg123 curl"));
+  xhr.send("cmd=touch&params="+stringToHex("/var/www/html/data/radio.stop"));
   toTop();
-//  window.scrollTo(0, 0);
-//  document.getElementById('miniclock').style.display = 'none';
-//  document.getElementById('minitemp').style.display = 'none';
   radioVolume(event, conf.tvvolume);
-  if (typeof event !== 'undefined') {
-    event.stopPropagation();
-  }
 }
 function radioVolume(event, command) {
   if (typeof event !== 'undefined') {
@@ -271,6 +279,20 @@ function radioVolume(event, command) {
       xhr.send("cmd=amixer&params="+stringToHex("set 'Digital' " + command + "% | awk -F'[][]' '/Left:/ { print $2 }'"));
   }
 }
+//function radioStatus() {
+//  var xhr = new XMLHttpRequest();
+//  xhr.open('POST', "cli.php", true);
+//  xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+//  xhr.onload = function(e) {
+//    if (this.status == 200) {
+//      const output = JSON.parse(this.responseText);
+//      if (output.length > 0) {
+//        document.getElementById("radioinfo").innerHTML = output[0];
+//      }
+//    }
+//  };
+//  xhr.send("cmd=cat&params="+stringToHex("/var/www/html/data/radio.log | tail -1 | cut -d \"'\" -f 2"));
+//}
 function radioStatus() {
   var xhr = new XMLHttpRequest();
   xhr.open('POST', "cli.php", true);
@@ -279,22 +301,49 @@ function radioStatus() {
     if (this.status == 200) {
       const output = JSON.parse(this.responseText);
       if (output.length > 0) {
-        document.getElementById("radioinfo").innerHTML = output[0];
+//        var fontsize = 10;
+        var status = output[0].substring(output[0].indexOf("='") + 2, output[0].indexOf("';"));
+//        if (status.length > 16) {
+//          fontsize = 5;
+//        }
+//        document.getElementById("radio").innerHTML = "<span style='font-size:" + fontsize + "vw'>" + status + "</span>";
+        document.getElementById("radioinfo").innerHTML = status;
+//      } else {
+//        document.getElementById("radio").innerHTML = "Wekkerradio " + conf.alarmclock.temp.toFixed(1) + " &#176;C";
       }
     }
   };
-  xhr.send("cmd=cat&params="+stringToHex("/var/www/html/data/radio.log | tail -1 | cut -d \"'\" -f 2"));
+  xhr.send("cmd=cat&params="+stringToHex("/var/www/html/data/radio.log | tail -1"));
 }
-function radioPlay(event, channel) {
-  if (typeof event !== 'undefined') {
-    event.stopPropagation();
-  }
+//function radioPlay(event, channel) {
+//  if (typeof event !== 'undefined') {
+//    event.stopPropagation();
+//  }
+//  var xhr = new XMLHttpRequest();
+//  xhr.open('POST', "cli.php", true);
+//  xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+//  xhr.send("cmd=killall&params="+stringToHex("mpg123 curl; sudo killall roc-recv;curl -H 'Icy-MetaData:1' --silent -L " + conf.radio.channel[channel].URL + " 2>&1 | mpg123 --icy-interval " + conf.radio.channel[channel].interval + " -f -12000 - 2> /var/www/html/data/radio.log"));
+//  document.getElementById("radioinfo").innerHTML = "Even geduld, de zenderinformatie wordt opgehaald...";
+//  setTimeout(function () { radioStatus(); }, 10000);
+//}
+function playRadio(event, cmd, channel) {
   var xhr = new XMLHttpRequest();
   xhr.open('POST', "cli.php", true);
   xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-  xhr.send("cmd=killall&params="+stringToHex("mpg123 curl; sudo killall roc-recv;curl -H 'Icy-MetaData:1' --silent -L " + conf.radio.channel[channel].URL + " 2>&1 | mpg123 --icy-interval " + conf.radio.channel[channel].interval + " -f -12000 - 2> /var/www/html/data/radio.log"));
-  document.getElementById("radioinfo").innerHTML = "Even geduld, de zenderinformatie wordt opgehaald...";
+  xhr.send("cmd=echo&params="+stringToHex("'" + cmd + ", 12000, " + conf.radio.channel[channel].interval + ", " + conf.radio.channel[channel].URL + "' > /var/www/html/data/radio.cmd"));
   setTimeout(function () { radioStatus(); }, 10000);
+}
+function radioPlay(event, cmd, channel) {
+  var xhr = new XMLHttpRequest();
+  xhr.open('POST', "cli.php", true);
+  xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  xhr.onload = function(e) {
+    if (this.status == 200) {
+      setTimeout(playRadio, 1000, event, cmd, channel);
+    }
+  };
+  xhr.send("cmd=touch&params="+stringToHex("/var/www/html/data/radio.stop"));
+  document.getElementById("radioinfo").innerHTML = "Even geduld, de zenderinformatie wordt opgehaald...";
 }
 var startTimer;
 var dayNames = new Array("Zondag","Maandag","Dinsdag","Woensdag","Donderdag","Vrijdag","Zaterdag");
@@ -1094,6 +1143,10 @@ function brightness() {
           if (this.status == 200) {
             const output = JSON.parse(this.responseText);
             current = output[0];
+//            var xhr = new XMLHttpRequest();
+//            xhr.open('POST', "cli.php", true);
+//            xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+//            xhr.send("cmd=echo&params="+stringToHex(backlight + " > /sys/class/backlight/rpi_backlight/brightness"));
             var xhr = new XMLHttpRequest();
             xhr.open('POST', "brightness.php", true);
             xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
