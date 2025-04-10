dfb5c86511d1f293e31e62fbca0c659481d05cd4
Dany Pinoy <dany.pinoy@pindanet.be>
2025-04-10 13:34:38 +0200

    Delete domoticaController/usr/sbin/PindaNetUpdate.sh

--- domoticaController/usr/sbin/PindaNetUpdate.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-if [ -f "/tmp/thermostatManual" ]; then
-  mv /tmp/thermostatManual /root/thermostatManual
-fi
-sudo dpkg --configure -a
-apt-get clean
-apt-get update
-apt-get upgrade -y
-shutdown -r now
