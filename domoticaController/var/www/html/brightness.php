6744f918e56b56c4db08d18edc734b5021b26626
Dany Pinoy <dany.pinoy@pindanet.be>
2025-04-10 13:58:41 +0200

    Update brightness.php

--- domoticaController/var/www/html/brightness.php
+++ domoticaController/var/www/html/brightness.php
@@ -1,4 +1,9 @@
 <?php
-$brightness = intval(htmlspecialchars($_POST["brightness"]));
-file_put_contents("/sys/class/backlight/10-0045/brightness", $brightness);
+if(isset($_POST['power'])){
+  $power = intval(htmlspecialchars($_POST["power"]));
+  file_put_contents("/sys/class/backlight/10-0045/bl_power", $power);
+} elseif (isset($_POST['brightness'])){
+  $brightness = intval(htmlspecialchars($_POST["brightness"]));
+  file_put_contents("/sys/class/backlight/10-0045/brightness", $brightness);
+}
 ?>
