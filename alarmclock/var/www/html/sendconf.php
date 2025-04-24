e59652047259d8845043c33145ffa2319c2ae6e7
Dany Pinoy <dany.pinoy@pindanet.be>
2025-04-24 18:01:33 +0200

    Update sendconf.php

--- alarmclock/var/www/html/sendconf.php
+++ alarmclock/var/www/html/sendconf.php
@@ -3,7 +3,8 @@ $json = file_get_contents('php://input');
 $data = json_decode($json, true);
 // Check if decoding was successful
 if ($data !== null) {
-  copy("data/conf.json","data/conf.json.bak");
+  rename("data/conf.json","data/conf.json.bak");
+  sleep(1);
   // Perform further processing or respond to the request
   if (file_put_contents("data/conf.json", $json))
     echo "Configuration is saved.";
