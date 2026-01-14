fd8036eaf47d9492f2eafe45b64a05acff784e17
Dany Pinoy <dany.pinoy@pindanet.be>
2026-01-14 15:20:07 +0100

    Update softap.sh

--- util/softap.sh
+++ util/softap.sh
@@ -1,5 +1,6 @@
 # rfkill list
 # sudo raspi-config
+key="GeheimWachtwoord"
 sudo nmcli connection del Wired\ connection\ 1
 sudo nmcli connection add type bridge con-name 'Bridge' ifname br0
 sudo nmcli connection add type ethernet slave-type bridge con-name 'Ethernet' ifname eth0 master br0
@@ -7,7 +8,7 @@ sudo nmcli connection add con-name 'PindaDomo' \
   ifname wlan0 type wifi slave-type bridge master br0 \
   wifi.mode ap wifi.ssid Proximus-Limited-388355 wifi-sec.key-mgmt wpa-psk \
   wifi-sec.proto rsn wifi-sec.pairwise ccmp \
-  wifi-sec.psk ywbzbunxr9p5jw5m
+  wifi-sec.psk $key
 
 sudo nmcli connection modify PindaDomo 802-11-wireless.band bg
 sudo nmcli connection modify PindaDomo 802-11-wireless.channel 11
