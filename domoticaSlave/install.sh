d1758a801573c99d82b5c2df770377fc556ce12d
Dany Pinoy <dany.pinoy@pindanet.be>
2026-02-11 18:30:49 +0100

    Update install.sh

--- domoticaSlave/install.sh
+++ domoticaSlave/install.sh
@@ -86,40 +86,7 @@ sudo usermod -a -G video www-data
 echo "Autostart fullscreen browser" # https://core-electronics.com.au/guides/raspberry-pi-kiosk-mode-setup/
 echo "============================"
 sudo apt install chromium -y
-cat > PindaNetAutostart.sh <<EOF
-#!/bin/bash
-# Disable Power led
-echo 0 | sudo tee /sys/class/leds/PWR/brightness
-# Disable Activity led
-echo none | sudo tee /sys/class/leds/ACT/trigger
-# Activate DS18B20 temperature sensor power (Reset)
-/usr/bin/pinctrl set $powergpio op dh
-# PullUp 1-wire Data
-/usr/bin/pinctrl set 4 ip pu
-# PullDown PIR1 and PIR2
-/usr/bin/pinctrl set $pir1gpio,$pir2gpio ip pd
-# Autostart Chromium in Kiosk & Debug mode
-/bin/chromium --remote-debugging-port=9222 --kiosk --disable-extensions --noerrdialogs --disable-infobars --enable-features=OverlayScrollbar  http://localhost/ &
-# Give Chromium time to start
-sleep 30
-# Check if Chromium is running
-until ps -ax | grep kiosk | grep -v grep
-do
-  # After a hostname change, chromium refuses to start, correct this
-  rm -rf $HOME/.config/chromium/Singleton*
-  # Restart chromium
-  /bin/chromium --remote-debugging-port=9222 --kiosk --disable-extensions --noerrdialogs --disable-infobars --enable-features=OverlayScrollbar  http://localhost/ &
-  sleep 30
-done
-EOF
-echo "/usr/bin/bash ~/PindaNetAutostart.sh &" >> .config/labwc/autostart
-
-# echo "Configure SSH remote login"
-# echo "=========================="
-# ssh-keygen
-# ssh-copy-id -i $HOME/.ssh/id_rsa.pub $(ls /home)@localhost
-# sudo cp .ssh/id_rsa /var/www/html/data/
-# sudo chown www-data:www-data /var/www/html/data/id_rsa
+echo "/usr/bin/bash /var/www/html/PindaNetAutostart.sh &" >> .config/labwc/autostart
 
 echo "Activate daily update"
 echo "====================="
