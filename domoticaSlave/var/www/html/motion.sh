30974bf7b20c2f1023e22c572be9f6937781f2a3
Dany Pinoy <dany.pinoy@pindanet.be>
2026-04-03 18:41:37 +0200

    Update motion.sh

--- domoticaSlave/var/www/html/motion.sh
+++ domoticaSlave/var/www/html/motion.sh
@@ -3,7 +3,7 @@
 # depends on jq
 # apt install jq
 
-# Eerst definiëren we enkele instellingen (constanten)
+# https://www.raspberrypi.com/documentation/computers/camera_software.html#post-processing-with-rpicam-apps
 motion=$(cat << EOF
 {
     "motion_detect" :
@@ -32,28 +32,24 @@ room=$(echo $jsonConf | jq --arg jq_hostname_var $HOSTNAME -r '.rooms.[] | selec
 read -r minBacklight maxBacklight timer< <(echo $room | jq -r '[ .minBacklight, .maxBacklight, .Motion.timer] | join(" ")')
 backlight=$minBacklight
 
-echo $minBacklight $maxBacklight $timer $backlight
-
 # Initialise flags
 if [ -f /tmp/timeTime ]; then
   rm /tmp/timeTime
 fi
 # Initialise touchscreen brightness
 echo 0 > /sys/class/backlight/10-0045/brightness
-# Daarna definiëren we enkele functies
 # Background action on motion
 action () {
   while :
   do
     endat=$(cat /tmp/timeTime)
-    echo $endat $(printf '%(%s)T\n' -1)
     if (($(printf '%(%s)T\n' -1) > $endat)); then
       break
     fi
     sleep 3
     endat=$(cat /tmp/timeTime)
   done
-  echo "Timeout"
+  echo "Timeout" $(date +"%Y-%m-%d_%H.%M.%S")
   rm /tmp/timeTime
   echo 0 > /sys/class/backlight/10-0045/brightness
 }
@@ -61,55 +57,52 @@ action () {
 wait_for_motion () {
   while read -r line ; do
     if [[ "$line" == *"Motion detected"* ]]; then
-      echo "Er is beweging."
+      echo "Motion detected" $(date +"%Y-%m-%d_%H.%M.%S")
       if ! [ -f /tmp/timeTime ]; then
-        echo $(($(printf '%(%s)T\n' -1) + 10)) > /tmp/timeTime # Tijd + 10 seconden
+        echo $(($(printf '%(%s)T\n' -1) + $timer)) > /tmp/timeTime # Tijd + 180 seconden
         break
       fi
       # reset timer
-      echo $(($(printf '%(%s)T\n' -1) + 10)) > /tmp/timeTime # Tijd + 10 seconden
-    elif [[ "$line" == *"Motion stopped"* ]]; then
-      echo "Er geen beweging."
+      echo $(($(printf '%(%s)T\n' -1) + $timer)) > /tmp/timeTime # Tijd + 180 seconden
+      echo "Reset timer" $(date +"%Y-%m-%d_%H.%M.%S")
+#    elif [[ "$line" == *"Motion stopped"* ]]; then
+#      echo "Er geen beweging."
     fi
   # We gebruiken --nopreview om zo onopvallend mogelijk beweging te detecteren.
   # Gebruik een preview om het detectiegebied te bekijken/af te regelen
   # Keep aspect ratio: 3280/16 = 205, 2464/16 = 154
   done < <(rpicam-hello -t 0 --nopreview --lores-width 205 --lores-height 154 --post-process-file /tmp/postProcess.json 2>&1)
 }
-# Hier begint ons programma
-# Oneindig lang worden bewegingen gemeld
-# Breek het programma af met Ctrl+c
+
 while :
 do
   wait_for_motion
-  echo "Backlight: $backlight"
+  # Temporary backlight
   echo $backlight > /sys/class/backlight/10-0045/brightness
   # Stop de bewegingsdetectie om de camera te gebruiken om een foto te maken
   pkill rpicam-hello
-  echo "Foto nemen."
-  bestandsnaam=$(date +"%Y-%m-%d_%H.%M.%S")
+#  echo "Take photo."
+  bestandsnaam=$(date +"%Y-%m-%d_%H:%M:%S")
   lux=$(sudo /usr/bin/rpicam-still -v 0 -o /tmp/$bestandsnaam".jpg" --rotation 180 --nopreview  --immediate --metadata - | grep '"Lux":')
   lux=${lux%.*} # until colon
   lux=${lux//[!0-9]/} # extract integer
-  echo "Lux: $lux"
+#  echo "Lux: $lux"
   if ! [ -f /var/www/html/data/luxmax ]; then
     luxmax=0
   else
     luxmax=$(cat /var/www/html/data/luxmax)
   fi
-  echo "Luxmax: $luxmax"
+#  echo "Luxmax: $luxmax"
   if (($lux > $luxmax)); then
     echo $lux > /var/www/html/data/luxmax
   fi
   let backlight=$lux*$maxBacklight/$luxmax+$minBacklight
-#  echo $backlight > /sys/class/backlight/10-0045/brightness
+  echo $backlight > /sys/class/backlight/10-0045/brightness
 
   find /var/www/html/motion -mmin +$((60*24)) -type f -delete
   /usr/bin/convert /tmp/$bestandsnaam".jpg" -resize 1920 /var/www/html/motion/$bestandsnaam".jpg"
 
-  echo /tmp/$bestandsnaam".jpg"
-  timerTime=$(printf '%(%s)T\n' -1)
-  date --date @$timerTime
+  rm /tmp/$bestandsnaam".jpg"
 
   action &
 done
