979da5d92689b9979e768d724fc25b3eb8dbd692
Dany Pinoy <dany.pinoy@pindanet.be>
2026-04-18 17:56:05 +0200

    Update install.sh

--- domoticaController/install.sh
+++ domoticaController/install.sh
@@ -141,8 +141,6 @@ sudo systemctl enable --now PindaNetUpdate.timer
 # Disable Avahi, use router DNS
 sudo systemctl disable --now avahi-daemon.service
 
-exit # Tot Hier
-
 # Check WiFi connection
 #sudo apt install avahi-utils -y
 sudo chmod +x /var/www/html/checkWiFi.sh
@@ -163,49 +161,7 @@ sudo systemctl daemon-reload
 sudo systemctl enable --now checkWiFi.timer
 # systemctl list-timers
 
-
-
-# Check Avahi hostname
-sudo apt install avahi-utils -y
-sudo tee /usr/sbin/checkAvahi.sh > /dev/null <<EOF
-#!/bin/bash
-# Check WiFi connection
-if ! ping -c 1 $router; then
-  echo "\$(date) Restart Network" >> /var/www/html/data/debug.txt
-  systemctl restart NetworkManager.service
-#  /sbin/shutdown -r now
-  sleep 10
-fi
-# Check Avahi conflict
-if [ \$(avahi-resolve -a \$(ip -4 addr show eth0 | grep -oP '(?<=inet\s)\d+(\.\d+){3}') | cut -f 2) != \${HOSTNAME}.local ]; then
-  echo Restart avahi
-  systemctl restart avahi-daemon.service
-fi
-EOF
-sudo chmod +x /usr/sbin/checkAvahi.sh
-
-sudo tee /etc/systemd/system/checkAvahi.timer > /dev/null <<EOF
-[Unit]
-Description=Check Avahi hostname
-[Timer]
-OnBootSec=5min
-OnUnitActiveSec=1h
-Unit=checkAvahi.service
-[Install]
-WantedBy=multi-user.target
-EOF
-
-sudo tee /etc/systemd/system/checkAvahi.service > /dev/null <<EOF
-[Unit]
-Description=Check Avahi hostname
-[Service]
-Type=simple
-ExecStart=/usr/sbin/checkAvahi.sh
-EOF
-
-sudo systemctl daemon-reload
-sudo systemctl enable checkAvahi.timer
-sudo systemctl start checkAvahi.timer
+exit # Tot Hier
 
 sudo chmod +x /var/www/html/ds18b20.sh
 sudo tee /etc/systemd/system/ds18b20.timer > /dev/null <<EOF
