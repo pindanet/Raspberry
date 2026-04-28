b2d62758b6dc97f8ac6a5bbe63386a0e56c45fd8
Dany Pinoy <dany.pinoy@pindanet.be>
2026-04-28 16:27:17 +0200

    Update sendConf.php.php

--- domoticaController/var/www/html/sendConf.php.php
+++ domoticaController/var/www/html/sendConf.php.php
@@ -1,12 +1,13 @@
 <?php
+$jsonName = "data/conf.php.json";
 $json = file_get_contents('php://input');
 $data = json_decode($json, true);
 // Check if decoding was successful
 if ($data !== null) {
-  copy("data/conf.php.json","data/conf.php.json.bak");
+  copy($jsonName, $jsonName . ".bak");
   sleep(1);
   // Perform further processing or respond to the request
-  if (file_put_contents("data/conf.php.json", $json))
+  if (file_put_contents($jsonName, $json))
     echo "Configuration is saved.";
   else 
     echo "Oops! Error creating json file...";
